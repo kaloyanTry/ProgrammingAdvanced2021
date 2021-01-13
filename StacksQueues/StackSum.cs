50247e45ab56056d160ca96825cacfded1133f02
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-13 15:31:23 +0200

    Stack sum operations

---
+++ StacksQueues/StackSum.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StackSum
+{
+    class StackSum
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Stack<int> stack = new Stack<int>(numbers);
+
+            string command = Console.ReadLine().ToLower();
+            while (command != "end")
+            {
+                if (command.Contains("add"))
+                {
+                    string[] splitted = command.Split();
+                    stack.Push(int.Parse(splitted[1]));
+                    stack.Push(int.Parse(splitted[2]));
+                }
+                else if (command.Contains("remove"))
+                {
+                    string[] splitted = command.Split();
+                    int count = int.Parse(splitted[1]);
+
+                    if (count <= stack.Count)
+                    {
+                        for (int i = 0; i < count; i++)
+                        {
+                            stack.Pop();
+                        }
+                    }
+                }
+
+                command = Console.ReadLine().ToLower();
+            }
+
+            Console.WriteLine("Sum: " + stack.Sum());
+        }
+    }
+}
