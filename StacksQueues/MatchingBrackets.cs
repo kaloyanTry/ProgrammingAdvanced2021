020703cb126129972374da2b02321e36bfbc172f
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-13 20:50:44 +0200

    Matching Brackets

---
+++ StacksQueues/MatchingBrackets.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MatchingBrackets
+{
+    class MatchingBrackets
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<int> bracetIndexes = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    bracetIndexes.Push(i);
+                }
+
+                if (input[i] == ')')
+                {
+                    int startIndex = bracetIndexes.Pop();
+                    Console.WriteLine(input.Substring(startIndex, i - startIndex + 1));
+                }
+            }
+        }
+    }
+}
