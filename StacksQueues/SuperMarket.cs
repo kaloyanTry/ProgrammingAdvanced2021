b99f46150d9928c5e085d768cf2ded8586841066
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-13 20:52:12 +0200

    Supermarket Queue

---
+++ StacksQueues/SuperMarket.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SuperMarket
+{
+    class SuperMarket
+    {
+        static void Main(string[] args)
+        {
+
+            Queue<string> namesQueue = new Queue<string>();
+            string inputNames = Console.ReadLine();
+            int countLeftInMarket = 0;
+
+            while (inputNames != "End")
+            {
+                
+                if (inputNames == "Paid")
+                {
+                    while (namesQueue.Any())
+                    {
+                        Console.WriteLine(namesQueue.Dequeue());
+                    }         
+                }
+                else
+                {
+                    namesQueue.Enqueue(inputNames);
+                }
+
+                
+                inputNames = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{namesQueue.Count} people remaining.");
+        }
+    }
+}
