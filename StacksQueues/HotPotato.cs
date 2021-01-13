7386703ee0293d214cd67e3b13c36cad569860e3
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-13 20:48:52 +0200

    Queue Enequeu, Dequeue
    Hot potatoes solution

---
+++ StacksQueues/HotPotato.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+namespace HotPotato
+{
+    class HotPotato
+    {
+        static void Main(string[] args)
+        {
+            string[] inputNames = Console.ReadLine().Split();
+
+            int n = int.Parse(Console.ReadLine());
+
+            Queue<string> kidsQueue = new Queue<string>(inputNames);
+
+            while (kidsQueue.Count != 1)
+            {
+                for (int i = 1; i < n; i++)
+                {
+                    kidsQueue.Enqueue(kidsQueue.Dequeue());
+
+                }
+                Console.WriteLine($"Removed {kidsQueue.Dequeue()}");
+            }
+
+            Console.WriteLine("Last is " + kidsQueue.Dequeue());
+        }
+    }
+}
