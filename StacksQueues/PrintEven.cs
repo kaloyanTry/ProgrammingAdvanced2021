b4fa602ed05290c94a956a56ae6d38372ae8df69
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-13 20:51:28 +0200

    Queue Print even number in int array

---
+++ StacksQueues/PrintEven.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PrintEvenNumbers
+{
+    class PrintEven
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> numsQueue = new Queue<int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    numsQueue.Enqueue(numbers[i]);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", numsQueue));
+        }
+    }
+}
