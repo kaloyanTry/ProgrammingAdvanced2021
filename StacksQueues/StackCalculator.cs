1b0014c1994401b153ee06a488677a17e10093e9
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-13 15:07:42 +0200

    SimpleCalculator

---
+++ StacksQueues/StackCalculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SimpleCalculator
+{
+    class StackCalculator
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            Stack<string> expression = new Stack<string>();
+
+            for (int i = input.Length - 1;  i >= 0; i--)
+            {
+                expression.Push(input[i]);
+            }
+
+            while (expression.Count > 1)
+            {
+                int leftNumber = int.Parse(expression.Pop());
+                string sign = expression.Pop();
+                int rightNumber = int.Parse(expression.Pop());
+
+
+                if (sign == "+")
+                {
+                    expression.Push((leftNumber + rightNumber).ToString());
+                }
+                else if (sign == "-")
+                {
+                    expression.Push((leftNumber - rightNumber).ToString());
+                }
+            }
+
+            Console.WriteLine(expression.Pop());
+        }
+    }
+}
