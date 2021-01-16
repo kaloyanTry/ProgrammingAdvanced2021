517c0a72cb4451ba44bf465cff63bf72e6f4ed0f
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-16 07:55:46 +0200

    Balanced Parantheses Stack

---
+++ StacksQueues/BalancedParantheses.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace BalancedParantheses
+{
+    class BalancedParantheses
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<char> brackets = new Stack<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '{')
+                {
+                    brackets.Push(input[i]);
+                }
+
+                if (input[i] == '[')
+                {
+                    brackets.Push(input[i]);
+                }
+
+                if (input[i] == '(')
+                {
+                    brackets.Push(input[i]);
+                }
+
+                if (input[i] == '}')
+                {
+                    if (brackets.Any() && brackets.Peek() == '{')
+                    {
+                        brackets.Pop();
+                    }
+                    else
+                    {
+                        Console.WriteLine("NO");
+                        return;
+                    }
+                }
+
+                if (input[i] == ']')
+                {
+                    if (brackets.Any() && brackets.Peek() == '[')
+                    {
+                        brackets.Pop();
+                    }
+                    else
+                    {
+                        Console.WriteLine("NO");
+                        return;
+                    }
+                }
+
+                if (input[i] == ')')
+                {
+                    if (brackets.Any() && brackets.Peek() == '(')
+                    {
+                        brackets.Pop();
+                    }
+                    else
+                    {
+                        Console.WriteLine("NO");
+                        return;
+                    }
+                }
+            }
+
+            if (brackets.Any())
+            {
+                Console.WriteLine("NO");
+            }
+            else
+            {
+                Console.WriteLine("YES");
+            }
+        }
+    }
+}
