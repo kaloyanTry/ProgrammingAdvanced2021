89d6a6d0099db588aa2d1930237018930971a970
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-16 07:40:17 +0200

    Basic Stack Operations

---
+++ StacksQueues/StackOperations.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BasicStackOperations
+{
+    class StackOperations
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] inputNums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int nPush = inputNums[0];
+            int sPop = inputNums[1];
+            int xContains = inputNums[2];
+
+            int[] inputArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> intStack = new Stack<int>();
+
+            for (int i = 0; i < nPush; i++)
+            {
+                intStack.Push(inputArray[i]);
+            }
+
+            for (int i = 0; i < sPop; i++)
+            {
+                intStack.Pop();
+            }
+
+            if (intStack.Count != 0)
+            {
+                if (intStack.Contains(xContains))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine(intStack.Min());
+                }
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+       
+        }
+    }
+}
