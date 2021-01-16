2b73241258afb6ad34a0c5dba067cefa5a8e139d
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-16 07:37:00 +0200

    FashionBoutiqueQueue

---
+++ StacksQueues/FashionBoutique.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace FashionBoutique
+{
+    class FashionBoutique
+    {
+        static void Main(string[] args)
+        {
+            int[] allClothesInBox = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Stack<int> allClothes = new Stack<int>(allClothesInBox);
+
+            int boxCapacity = int.Parse(Console.ReadLine());
+
+            int currentRackCapacity = boxCapacity;
+            int racksCount = 1;
+
+            while (allClothes.Any())
+            {
+                int clothes = allClothes.Pop();
+
+                currentRackCapacity -= clothes;
+
+                if (currentRackCapacity < 0)
+                {
+                    racksCount++;
+                    currentRackCapacity = boxCapacity - clothes;
+                }
+            }
+
+            Console.WriteLine(racksCount);
+        }
+    }
+}
