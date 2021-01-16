48f94d56108329788a8ebead1afd5bb9ac1bd7eb
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-16 07:36:24 +0200

    FastFoodQueue

---
+++ StacksQueues/FastFood.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace FastFood
+{
+    class FastFood
+    {
+        static void Main(string[] args)
+        {
+            int quantityForDay = int.Parse(Console.ReadLine());
+
+
+            Queue<int> ordersQueue = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+
+            Console.WriteLine(ordersQueue.Max());
+
+            while (ordersQueue.Count > 0)
+            {
+                int order = ordersQueue.Peek();
+
+                if (quantityForDay >= order)
+                {
+                    quantityForDay -= order;
+                    ordersQueue.Dequeue();
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (ordersQueue.Count == 0)
+            {
+                Console.WriteLine("Orders complete");
+            }
+            else
+            {
+                Console.WriteLine("Orders left: " + string.Join(" ", ordersQueue));
+            }
+        }
+    }
+}
