840853b7688ebff9ae782140ee7f9564ec0148ba
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-16 07:39:43 +0200

    Basic Queue operations

---
+++ StacksQueues/BasicQueus.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace BasicQueueOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputNums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int nPush = inputNums[0];
+            int sPop = inputNums[1];
+            int xContains = inputNums[2];
+
+            int[] inputArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> intQueue = new Queue<int>();
+
+            for (int i = 0; i < nPush; i++)
+            {
+                intQueue.Enqueue(inputArray[i]);
+            }
+
+            for (int i = 0; i < sPop; i++)
+            {
+                intQueue.Dequeue();
+
+            }
+
+            if (intQueue.Contains(xContains))
+            {
+                Console.WriteLine("true");
+            }
+            else if (intQueue.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                Console.WriteLine(intQueue.Min());
+            }
+
+        }
+    }
+}
