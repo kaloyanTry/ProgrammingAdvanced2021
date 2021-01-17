082e64e07dc6a18ea87d811aa9e723001871ab58
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-17 20:40:05 +0200

    CupBottles

---
+++ StacksQueues/CupsBottles.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CupsBottles
+{
+    class CupsBottles
+    {
+        static void Main(string[] args)
+        {
+            int[] cups = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] bottles = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int wastedWater = 0;
+
+            Queue<int> cupsCapacity = new Queue<int>(cups);
+            Stack<int> bottlesCapacity = new Stack<int>(bottles);
+
+            while (cupsCapacity.Any() && bottlesCapacity.Any())
+            {
+                int cup = cupsCapacity.Peek();
+                int currentCupLevel = 0;
+                while (currentCupLevel < cup && bottlesCapacity.Any())
+                {
+                    int bottle = bottlesCapacity.Pop();
+                    currentCupLevel += bottle;
+                }
+
+                cupsCapacity.Dequeue();
+                wastedWater += currentCupLevel - cup;
+            }
+
+            if (!cupsCapacity.Any())
+            {
+                Console.WriteLine("Bottles: " + string.Join(" ", bottlesCapacity));
+            }
+            else if (!bottlesCapacity.Any())
+            {
+                Console.WriteLine("Cups: " + string.Join(" ", cupsCapacity));
+            }
+
+            Console.WriteLine("Wasted litters of water: " + wastedWater);
+        }
+    }
+}
