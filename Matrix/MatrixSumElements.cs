0a8606d4a90904e76f5cc7b9c42482315af504ab
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-17 20:48:33 +0200

    Sum elements of matrix

---
+++ Matrix/MatrixSumElements.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SumElementsMatrix
+{
+    class MatrixSumElements
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = row + col;
+                }
+            }
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
