0730ecaba6ccb9bfe471eff89a31bd2c0fab5e84
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-19 10:49:06 +0200

    Snake for rows cols +-

---
+++ Matrix/SnakeName2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SnakeName
+{
+    class SnakeName2
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            int rows = input[0];
+            int cols = input[1];
+            char[,] matrix = new char[rows, cols];
+
+            string snakeName = Console.ReadLine();
+            int currentLetter = 0;
+
+            for (int row = 0; row < rows; row++)
+            {
+                if (row % 2 == 0)
+                {
+                    for (int col = 0; col < cols; col++)
+                    {
+                        matrix[row, col] = snakeName[currentLetter];
+                        currentLetter++;
+                        if (currentLetter == snakeName.Length)
+                        {
+                            currentLetter = 0;
+                        }
+                    }
+                }
+                else
+                {
+                    for (int col = cols - 1; col >= 0; col--)
+                    {
+                        matrix[row, col] = snakeName[currentLetter];
+                        currentLetter++;
+                        if (currentLetter == snakeName.Length)
+                        {
+                            currentLetter = 0;
+                        }
+                    }
+                }
+            }
+
+            printMatrix(matrix);
+        }
+
+
+        private static void printMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
