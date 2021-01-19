351ec712bd423d1163612405c7d16a0ff4f341c6
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-19 10:52:25 +0200

    Equal matrix

---
+++ Matrix/2x2SquaresInMatrix.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _2x2SquaresInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputData = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int rows = inputData[0];
+            int cols = inputData[1];
+
+            char[,] matrix = new char[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                char[] rowData = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = rowData[col];
+                }
+            }
+
+            int equalMatrixes = 0;
+
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    if (matrix[row, col] == matrix[row, col + 1] &&
+                        matrix[row, col] == matrix[row + 1, col + 1] &&
+                        matrix[row, col] == matrix[row + 1, col])
+                    {
+                        equalMatrixes++;
+                    }
+
+                }
+            }
+
+            Console.WriteLine(equalMatrixes);
+        }
+    }
+}
