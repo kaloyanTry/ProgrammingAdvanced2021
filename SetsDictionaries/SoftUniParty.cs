b61f60f54560e61d71d0904ec9d01b7c14a460ba
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-20 08:33:36 +0200

    SoftUni Party HashSets: any, contains, add, remove

---
+++ SetsDictionaries/SoftUniParty.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SoftUniParty
+{
+    class SoftUniParty
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> vipGuests = new HashSet<string>();
+            HashSet<string> regularGuests = new HashSet<string>();
+
+            string input = Console.ReadLine();
+
+            while (input != "PARTY")
+            {
+                if (input[0] >= '0' && input[0] <= '9')
+                {
+                    vipGuests.Add(input);
+                }
+                else
+                {
+                    regularGuests.Add(input);
+                }
+                input = Console.ReadLine();
+            }
+
+            int allGuests = vipGuests.Count + regularGuests.Count;
+            input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                if (input[0] >= '0' && input[0] <= '9')
+                {
+                    if (vipGuests.Contains(input))
+                    {
+                        vipGuests.Remove(input);
+                        allGuests--;
+                    }
+                }
+                else
+                {
+                    if (regularGuests.Contains(input))
+                    {
+                        regularGuests.Remove(input);
+                        allGuests--;
+                    }
+                }
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(allGuests);
+            if (vipGuests.Any())
+            {
+                Console.WriteLine(string.Join('\n', vipGuests));
+            }
+            if (regularGuests.Any())
+            {
+                Console.WriteLine(string.Join('\n', regularGuests));
+            }
+        }
+    }
+}
