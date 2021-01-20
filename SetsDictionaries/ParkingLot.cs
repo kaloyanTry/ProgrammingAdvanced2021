22e7f5b5f386c413efbc88a7c223c0a3b19fdcb2
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-20 08:12:21 +0200

    HashSet add, remove

---
+++ SetsDictionaries/ParkingLot.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ParkingLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> carNumbers = new HashSet<string>();
+            string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                string[] splitted = input.Split(", ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                string command = splitted[0];
+                string carNumber = splitted[1];
+
+                if (command == "IN")
+                {
+                    if (!carNumbers.Contains(carNumber))
+                    {
+                        carNumbers.Add(carNumber);
+                    }
+                }
+
+                if (command == "OUT")
+                {
+                    if (carNumbers.Contains(carNumber))
+                    {
+                        carNumbers.Remove(carNumber);
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+            if (carNumbers.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+
+            foreach (var car in carNumbers)
+            {
+                Console.WriteLine(car);
+            }
+        }
+    }
+}
