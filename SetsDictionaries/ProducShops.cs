0d05bb9ef8f4d6a4195c7624a741a48a4d0213ad
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-20 08:13:39 +0200

    Dictionaries in Dictionary

---
+++ SetsDictionaries/ProducShops.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ProductsShops
+{
+    class ProducShops
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, double>> shops = new Dictionary<string, Dictionary<string, double>>();
+
+            string input = Console.ReadLine();
+
+            while (input != "Revision")
+            {
+                string[] inData = input.Split(", ");
+                string shop = inData[0];
+                string product = inData[1];
+                double price = double.Parse(inData[2]);
+
+                if (!shops.ContainsKey(shop))
+                {
+                    shops.Add(shop, new Dictionary<string, double>());
+                }
+
+                if (!shops[shop].ContainsKey(product))
+                {
+                    shops[shop].Add(product, price);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            shops = shops.OrderBy(s => s.Key).ToDictionary(k => k.Key, v => v.Value);
+
+            foreach (var shop in shops)
+            {
+                Console.WriteLine($"{shop.Key}->");
+
+                foreach (var product in shop.Value)
+                {
+                    Console.WriteLine($"Product: {product.Key}, Price: {product.Value}");
+                }
+            }
+        }
+    }
+}
