1acfe0742cbd855bd017862c3a5d5e51466ec83c
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-20 19:08:06 +0200

    Update SquareSum.cs

--- Matrix/SquareSum.cs
+++ Matrix/SquareSum.cs
@@ -8,49 +8,60 @@ namespace SquareSum
     {
         static void Main(string[] args)
         {
-            int[] input = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int[] input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
             int rows = input[0];
             int cols = input[1];
 
             int[,] matrix = new int[rows, cols];
 
-            for (int row = 0; row < rows; row++)
-            {
-                int[] rowData = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
-                for (int col = 0; col < cols; col++)
-                {
-                    matrix[row, col] = rowData[col];
-                }
-            }
+            FillMatrix(rows, cols, matrix);
 
             int maxSum = int.MinValue;
-            int maxSumRow = 0;
-            int maxSumCol = 0;
+            int maxRow = -1;
+            int maxCol = -1;
 
             for (int row = 0; row < rows - 1; row++)
             {
                 for (int col = 0; col < cols - 1; col++)
                 {
-                    int squareSum = matrix[row, col] + matrix[row, col + 1] + matrix[row + 1, col] + matrix[row + 1, col + 1];
-
-                    if (squareSum > maxSum)
+                    int currentSum = matrix[row, col] + matrix[row, col + 1] + matrix[row + 1, col] + matrix[row + 1, col + 1];
+                    if (currentSum > maxSum)
                     {
-                        maxSum = squareSum;
-                        maxSumRow = row;
-                        maxSumCol = col;
+                        maxSum = currentSum;
+                        maxRow = row;
+                        maxCol = col;
                     }
                 }
             }
-            
-            for (int row = maxSumRow; row < maxSumRow + 2; row++)
+
+            PrintMatrix(matrix, maxRow, maxCol);
+
+            Console.WriteLine(maxSum);
+        }
+
+        private static void PrintMatrix(int[,] matrix, int maxRow, int maxCol)
+        {
+            for (int row = maxRow; row <= maxRow + 1; row++)
             {
-                for (int col = maxSumCol; col < maxSumCol + 2; col++)
+                for (int col = maxCol; col <= maxCol + 1; col++)
                 {
                     Console.Write(matrix[row, col] + " ");
                 }
                 Console.WriteLine();
             }
-            Console.WriteLine(maxSum);
+        }
+
+        private static void FillMatrix(int rows, int cols, int[,] matrix)
+        {
+            for (int row = 0; row < rows; row++)
+            {
+                int[] rowData = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = rowData[col];
+                }
+            }
         }
     }
 }
