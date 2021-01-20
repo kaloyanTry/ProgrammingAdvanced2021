b9fb6b8a7d18c5fdbb17868ec312d20082592240
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-20 08:16:31 +0200

    Dictionary Linq Order

---
+++ SetsDictionaries/AvrgStudentsGrades.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AverageStudentsGrades
+{
+    class AvrgStudentsGrades
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, List<decimal>> studentsGrades = new Dictionary<string, List<decimal>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var student = Console.ReadLine().Split();
+                string name = student[0];
+                decimal grade = decimal.Parse(student[1]);
+
+                if (!studentsGrades.ContainsKey(name))
+                {
+                    studentsGrades.Add(name, new List<decimal>());
+                }
+
+                studentsGrades[name].Add(grade);
+            }
+
+            foreach (var student in studentsGrades)
+            {
+                Console.Write($"{student.Key} -> ");
+
+                foreach (var grade in student.Value)
+                {
+                    Console.Write($"{grade:F2} ");
+                }
+                Console.WriteLine($"(avg: {student.Value.Average():F2})");
+            }
+        }
+    }
+}
