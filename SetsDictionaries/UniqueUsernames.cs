db2438e2429eb6d395ce77d1c1c1272637052f4e
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-21 21:52:01 +0200

    HAshSet

---
+++ SetsDictionaries/UniqueUsernames.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UniqueUsernames
+{
+    class UniqueUsernames
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            HashSet<string> usernames = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                usernames.Add(name);
+            }
+
+            foreach (var name in usernames)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
