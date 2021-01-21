75d37c0c64dedc26ea788927af959e9f9c832c5d
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-21 21:54:37 +0200

    HashSet string

---
+++ SetsDictionaries/PeriodicTable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace PeriodicTable
+{
+    class PeriodicTable
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            HashSet<string> tableElements = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split().ToArray();
+
+                for (int j = 0; j < input.Length; j++)
+                {
+                    tableElements.Add(input[j]);
+                }
+            }
+
+            tableElements = tableElements.OrderBy(e => e).ToHashSet();
+
+            Console.WriteLine(string.Join(" ", tableElements));
+        }
+    }
+}
