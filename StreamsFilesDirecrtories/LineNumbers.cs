6d44f890ae275895100d349e970f7581148ccae3
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-22 21:52:58 +0200

    Add files via upload

---
+++ StreamsFilesDirecrtories/LineNumbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace LineNumbers
+{
+    class LineNumbers
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("../../../input.txt"))
+            {
+                using (StreamWriter writer = new StreamWriter("../../../output.txt"))
+                {
+                    string input = reader.ReadLine();
+                    int row = 1;
+                    while (input != null)
+                    {
+                        writer.WriteLine($"{row}. {input}");
+                        row++;
+                        input = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
