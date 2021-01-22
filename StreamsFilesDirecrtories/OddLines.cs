f78dcd279521b95d63e446465eb7f4fe7c0be360
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-22 21:21:05 +0200

    Stream Read and Write

---
+++ StreamsFilesDirecrtories/OddLines.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace OddLines
+{
+    class OddLines
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("../../../input.txt"))
+            {
+                string currentRow = reader.ReadLine();
+                int row = 0;
+
+                using (StreamWriter writer = new StreamWriter("../../../output.txt"))
+                {
+                    while (currentRow != null)
+                    {
+                        if (row % 2 == 1)
+                        {
+                            writer.WriteLine(currentRow);
+                        }
+
+                        currentRow = reader.ReadLine();
+                        row++;
+                    }
+                }
+            }
+        }
+    }
+}
