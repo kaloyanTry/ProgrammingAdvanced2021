b17c7300d4ef0cee9de21c6bcd843a7932ed0ed0
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-23 08:44:08 +0200

    File.ReadAllText

---
+++ StreamsFilesDirecrtories/SliceFile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace SliceFile
+{
+    class SliceFile
+    {
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText("../../../sliceMe.txt");
+
+            int fileLenght = (int)Math.Ceiling((double)text.Length / 4);
+
+            int charsWritten = 0;
+
+            for (int i = 1; i <= 4; i++)
+            {
+                using (StreamWriter writer = new StreamWriter("../../../slice-" + i + ".txt"))
+                {
+                    writer.Write(text.ToCharArray(), charsWritten, fileLenght);
+                    charsWritten += fileLenght - 1;
+                }
+            }
+        }
+    }
+}
