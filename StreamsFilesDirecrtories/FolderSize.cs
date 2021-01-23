7997b7ecee7e1bb7de96ba7ecc4f95d21e18dc7e
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-23 08:43:14 +0200

    FileInfo Directory

---
+++ StreamsFilesDirecrtories/FolderSize.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace FolderSize
+{
+    class FolderSize
+    {
+        static void Main(string[] args)
+        {
+            string[] files = Directory.GetFiles("../../../TestFolder");
+            double totalSizeMB = 0;
+
+            foreach (var file in files)
+            {
+                FileInfo fileInfo = new FileInfo(file);
+                double fileSizeMB = (double)fileInfo.Length / 1024 / 1024;
+                totalSizeMB += fileSizeMB;
+            }
+
+            File.WriteAllText("../../../output.txt", totalSizeMB.ToString());
+        }
+    }
+}
