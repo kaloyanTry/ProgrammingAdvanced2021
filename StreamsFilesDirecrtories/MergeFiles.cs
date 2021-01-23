cc5313e223f81e09d5fa9bfdc63a96279f331786
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-23 08:45:05 +0200

    StreamReader StreamWriter

---
+++ StreamsFilesDirecrtories/MergeFiles.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace MergeFiles
+{
+    class MergeFiles
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = new List<int>();
+            using (StreamReader reader = new StreamReader("../../../FileOne.txt"))
+            {
+                string input = reader.ReadLine();
+                while (input != null)
+                {
+                    int num = int.Parse(input);
+
+                    nums.Add(num);
+
+                    input = reader.ReadLine();
+                }
+            }
+
+            using (StreamReader reader = new StreamReader("../../../FileTwo.txt"))
+            {
+                string input = reader.ReadLine();
+                while (input != null)
+                {
+                    int num = int.Parse(input);
+
+                    nums.Add(num);
+
+                    input = reader.ReadLine();
+                }
+            }
+
+            nums.Sort();
+
+            using (StreamWriter writer = new StreamWriter("../../../Output.txt"))
+            {
+                foreach (var num in nums)
+                {
+                    writer.WriteLine(num);
+                }
+            }
+        }
+    }
+}
