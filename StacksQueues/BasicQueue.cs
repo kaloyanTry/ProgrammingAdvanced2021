f4a8100cea03e7bf08b627c0ef5b398cfc0dd6f7
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-24 21:28:20 +0200

    Update and rename BasicQueus.cs to BasicQueue.cs

---
+++ StacksQueues/BasicQueue.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace BasicQueueOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int numEnque = input[0];
+            int numDeque = input[1];
+            int numLook = input[2];
+
+            int[] numsInput = Console.ReadLine().Split().Select(int.Parse).ToArray();
+           
+            int smallestNum = int.MaxValue;
+            Queue<int> nums = new Queue<int>();
+
+            for (int i = 0; i < numEnque; i++)
+            {
+                nums.Enqueue(numsInput[i]);
+            }
+
+            for (int j = 0; j < numDeque; j++)
+            {
+                nums.Dequeue();
+            }
+
+            if (nums.Contains(numLook))
+            {
+                Console.WriteLine("true");
+            }
+            else if (nums.Count == 0)
+            {
+                Console.WriteLine("0");
+            }
+            else
+            {
+                smallestNum = nums.Min();
+                Console.WriteLine(smallestNum);
+            }
+        }
+    }
+}
