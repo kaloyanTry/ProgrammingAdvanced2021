293a4f61b261b702fa6e4127797f4822bd036785
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-24 08:26:38 +0200

    Simple StreamReader and StreamWriter

---
+++ StreamsFilesDirecrtories/StreamReaderWriter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleStream
+{
+    class StreamReaderWriter
+    {
+        static void Main(string[] args)
+        {
+            //Streamreader:
+            StreamReader reader = new StreamReader("../../../input.txt");
+
+            string line = reader.ReadToEnd();
+            //string alLines = reader.ReadToEnd();
+
+            Console.WriteLine(line);
+
+            reader.Close();
+
+            //Streamwriter:
+            StreamWriter writer = new StreamWriter("../../../input.txt", true);
+
+            writer.WriteLine("We are the champs!");
+
+            writer.Close();
+
+            //USING: the same functionality but automaticly close the reader
+            //Always use USING! This is a good practice
+
+            //using (StreamReader readerStream = new StreamReader("../../../input.txt"))
+            //{
+            //    string streamLine = reader.ReadToEnd();
+            //    Console.WriteLine(line);
+            //}
+        }
+    }
+}
