3c97b590e7eddf6329f4c4953571184f0706866f
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-24 20:42:33 +0200

    Update BasicQueus.cs

--- StacksQueues/BasicQueus.cs
+++ StacksQueues/BasicQueus.cs
@@ -9,11 +9,13 @@ namespace BasicQueueOperations
         static void Main(string[] args)
         {
             int[] inputNums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            
             int nPush = inputNums[0];
             int sPop = inputNums[1];
             int xContains = inputNums[2];
 
             int[] inputArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            
             Queue<int> intQueue = new Queue<int>();
 
             for (int i = 0; i < nPush; i++)
@@ -24,7 +26,6 @@ namespace BasicQueueOperations
             for (int i = 0; i < sPop; i++)
             {
                 intQueue.Dequeue();
-
             }
 
             if (intQueue.Contains(xContains))
