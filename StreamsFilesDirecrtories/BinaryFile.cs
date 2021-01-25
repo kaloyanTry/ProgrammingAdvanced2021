42db6dcf203cc4a9f5d677bd81cb37843f67b796
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-25 21:46:48 +0200

    Add files via upload

---
+++ StreamsFilesDirecrtories/BinaryFile.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace CopyBinaryFile
+{
+    class BinaryFile
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream readStream = new FileStream("../../../copyMe.png", FileMode.Open))
+            {
+                using (FileStream writeStream = new FileStream("../../../copiedImage.png", FileMode.Create))
+                {
+                    while (true)
+                    {
+                        byte[] buffer = new byte[4096];
+                        int readBuffer = readStream.Read(buffer, 0, buffer.Length);
+
+                        if (readBuffer == 0)
+                        {
+                            break;
+                        }
+
+                        writeStream.Write(buffer, 0, readBuffer);
+                    }
+
+                }
+            }
+                
+
+
+        }
+    }
+}
