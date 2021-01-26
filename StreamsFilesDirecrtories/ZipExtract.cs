01e0357995da239513d229174538389ed5c97f5b
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-26 11:29:05 +0200

    ZipFIle Extract

---
+++ StreamsFilesDirecrtories/ZipExtract.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace ZipExtract
+{
+    class ZipExtract
+    {
+        static void Main(string[] args)
+        {
+            var zipFile = "../../../myzip.zip";
+            var file = "../../../copyMe.png";
+
+            using (var archive = ZipFile.Open(zipFile, ZipArchiveMode.Create))
+            {
+                archive.CreateEntryFromFile(file, Path.GetFileName(file));
+            }
+
+        }
+    }
+}
