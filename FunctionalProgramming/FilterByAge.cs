3b548c1947b9e485538cf30b06892aaf7ab33400
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-27 20:42:16 +0200

    Add files via upload

---
+++ FunctionalProgramming/FilterByAge.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace FilterByAge
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    class FilterByAge
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Person[] person = new Person[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+                person[i] = new Person();
+                person[i].Name = input[0];
+                person[i].Age = int.Parse(input[1]);
+
+
+            }
+
+            string filter = Console.ReadLine();
+            int filterAge = int.Parse(Console.ReadLine());
+
+            Func<Person, bool> condition = GetAgeCondition(filter, filterAge);
+
+            Action<Person> formatter = GetFormatter(Console.ReadLine());
+
+            PrintPersons(person, condition, formatter);
+        }
+
+        static Action<Person> GetFormatter(string format)
+        {
+            switch(format)
+            {
+                case "name":
+                    return p => { Console.WriteLine($"{p.Name}"); };
+                case "age":
+                    return p => { Console.WriteLine($"{p.Age}"); };
+                case "name age":
+                    return p => { Console.WriteLine($"{p.Name} - {p.Age}"); };
+                default:
+                    return null;
+            }
+        }
+
+        static Func<Person, bool> GetAgeCondition(string filter, int filterAge)
+        {
+            switch (filter)
+            {
+                case "younger": return p => p.Age < filterAge;
+                case "older": return p => p.Age >= filterAge;
+                default:
+                    return null;
+            }
+        }
+
+        static void PrintPersons(Person[] perosons, Func<Person, bool> condition, Action<Person> formatter)
+        {
+            foreach (var person in perosons)
+            {
+                if (condition(person))
+                {
+                    formatter(person);
+                }
+
+            }
+        }
+    }
+}
