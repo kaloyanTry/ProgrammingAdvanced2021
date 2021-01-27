2773ae5f892d84d0236b0e05ce14c4691529708b
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-27 20:42:36 +0200

    Add files via upload

---
+++ FunctionalProgramming/EvenNumbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace EvenNumbers
+{
+    class EvenNumbers
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(n => int.Parse(n)).Where(n => n % 2 == 0).OrderBy(n => n).ToArray();
+
+            string evenArray = (string.Join(", ", array));
+
+            Console.WriteLine(evenArray);
+        }
+    }
+}
