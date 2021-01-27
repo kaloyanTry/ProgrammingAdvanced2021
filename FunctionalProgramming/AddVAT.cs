7ac5b6bca0ae656919c9558d1eea36bbfcf4a0db
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-27 20:43:19 +0200

    Add files via upload

---
+++ FunctionalProgramming/AddVAT.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace AddVAT
+{
+    class AddVAT
+    {
+        static void Main(string[] args)
+        {
+            decimal[] numbers = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(decimal.Parse)
+                .Select(n => n +(n * 0.2m)).ToArray();
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine($"{number:F2}");
+            }
+        }
+    }
+}
