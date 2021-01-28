021f5fb15f6f13d60a14e4b3c5aabd55d2da3c68
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-28 22:23:54 +0200

    Add files via upload

---
+++ FunctionalProgramming/ListPredicate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListPredicates
+{
+    class ListPredicate
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] dividers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).Distinct().ToArray();
+
+            Func<int, int, bool> predicate = (num, d) => num % d == 0;
+
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (dividers.All(d => predicate(i, d)))
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
