d80b6b1d613308e01a3bbdd213d358fc26ede5ed
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-28 22:24:20 +0200

    Add files via upload

---
+++ FunctionalProgramming/PredicateForNames.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PredicateForName
+{
+    class PredicateForNames
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> names = Console.ReadLine().Split(' ').ToList();
+
+            Func<string, bool> func = name => name.Length <= n;
+
+            names = names.Where(func).ToList();
+
+            Action<List<string>> print = names => Console.WriteLine(string.Join(Environment.NewLine, names));
+
+            print(names);
+        }
+
+        static List<int> MyWhere(List<int> numbers, Predicate<int> predicate)
+        {
+            List<int> newNumbers = new List<int>();
+
+            foreach (int currNum in numbers)
+            {
+                if (predicate(currNum))
+                {
+                    newNumbers.Add(currNum);
+                }
+            }
+            return newNumbers;
+        }
+            
+    }
+}
