7aca873aab3b88e70fb718ae95b1476e862a848e
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-28 22:22:08 +0200

    Add files via upload

---
+++ FunctionalProgramming/AppliedArithmetics.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AppliedArithmetics
+{
+    class AppliedArithmetics
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToList();
+
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                Func<int, int> applyOperation = CreateOperation(command);
+
+                if (command != "print")
+                {
+                    numbers = numbers.Select(applyOperation).ToList();
+                }
+                else
+                {
+                    Console.WriteLine(string.Join(" ", numbers));
+                }
+
+                command = Console.ReadLine();
+            }
+        }
+
+        private static Func<int, int> CreateOperation(string command)
+        {
+            if (command == "add")
+            {
+                return x => x + 1;
+            }
+            if (command == "subtract")
+            {
+                return x => x - 1;
+            }
+
+            return x => x * 2;
+        }
+    }
+}
