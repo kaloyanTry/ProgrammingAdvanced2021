a992075848f9bcbcc28b1a6e0ccf8e0572eada1b
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-28 22:23:30 +0200

    Add files via upload

---
+++ FunctionalProgramming/KnightsHonor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace KnightsHonor
+{
+    class KnightsHonor
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            Action<List<string>> printer = x => Console.WriteLine(string.Join(Environment.NewLine, x.Select(y => $"Sir {y}")));
+
+            printer(names);
+        }
+    }
+}
