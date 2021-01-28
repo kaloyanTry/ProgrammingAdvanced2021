aa8e84ddc25a23cd70e9154f59ad5cfe3b3883e7
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-28 22:21:51 +0200

    Add files via upload

---
+++ FunctionalProgramming/ActionPoint.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ActionPoint
+{
+    class ActionPoint
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            Action<string> printer = x => Console.WriteLine(x);
+
+            names.ForEach(printer);
+        }
+    }
+}
