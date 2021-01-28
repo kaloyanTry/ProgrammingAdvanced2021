fb185caa4c267fb5c58940b2bbc2f5fbe2d5c70d
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-28 22:23:11 +0200

    Add files via upload

---
+++ FunctionalProgramming/FindEvensOdds.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace FindEvensOdds
+{
+    class FindEvensOdds
+    {
+        static void Main(string[] args)
+        {
+            int[] bounds = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            string condition = Console.ReadLine();
+
+            Func<int, bool> filter = CreateEvenOddFilter(condition);
+
+            for (int i = bounds[0]; i <= bounds[1]; i++)
+            {
+                if (filter(i))
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+
+        private static Func<int, bool> CreateEvenOddFilter(string condition)
+        {
+            if (condition == "even")
+            {
+                return x => x % 2 == 0;
+            }
+
+            return x => x % 2 != 0;
+        }
+    }
+}
