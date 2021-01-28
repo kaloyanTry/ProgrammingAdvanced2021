4a3c2f5984d0bf39bd44d1444fa613d2845a716a
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-28 22:22:50 +0200

    Add files via upload

---
+++ FunctionalProgramming/CustomMinFunc.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CustomMinFunc
+{
+    class CustomMinFunc
+    {
+        static void Main(string[] args)
+        {
+            Func<List<int>, int> minNumber = numbers =>
+            {
+                int minNum = int.MaxValue;
+                foreach (int num in numbers)
+                {
+                    if (num < minNum)
+                    {
+                        minNum = num;
+                    }
+                }
+
+                return minNum;
+            };
+
+            List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            int numNumber = minNumber(numbers);
+            Console.WriteLine(numNumber);
+        }
+    }
+}
