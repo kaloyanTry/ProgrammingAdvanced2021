b68c2e7ee3f1fe3b631a1f67cd01b6fe837c70dd
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-30 10:13:34 +0200

    Add files via upload

---
+++ DifiningClasses/SpecialCars/Car.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        public Car()
+        {
+            Make = "VW";
+            Model = "Golf";
+            Year = 2025;
+            FuelQuantity = 200;
+            FuelConsumption = 10;
+        }
+
+        public Car(string make, string model, int year) : this()
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption) : this(make, model, year)
+        {
+            FuelQuantity = fuelQuantity;
+            FuelConsumption = fuelConsumption;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption, Engine engine, Tire[] tires)
+            : this(make, model, year, fuelQuantity, fuelConsumption)
+        {
+            Engine = engine;
+            Tires = tires;
+        }
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public double FuelQuantity { get; set; }
+
+        public double FuelConsumption { get; set; }
+
+        public Engine Engine { get; set; }
+
+        public Tire[] Tires { get; set; }
+
+        public void Drive (double distance)
+        {
+            if (FuelQuantity - (distance / 100 * FuelConsumption) >= 0)
+            {
+                FuelQuantity -= (distance / 100 * FuelConsumption);
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        //public string WhoAmI()
+        //{
+        //    return $"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nFuel: {this.FuelQuantity:F2}L";
+        //}
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Make: {Make}");
+            sb.AppendLine($"Model: {Model}");
+            sb.AppendLine($"Year: {Year}");
+            sb.AppendLine($"HorsePowers: {Engine.HorsePower}");
+            sb.AppendLine($"FuelQuantity: {FuelQuantity}");
+            return sb.ToString();
+        }
+
+    }
+}
---
+++ DifiningClasses/SpecialCars/StartUp.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Tire[]> tires = new List<Tire[]>();
+
+            string command = Console.ReadLine();
+            while (command != "No more tires")
+            {
+                string[] tiresData = command.Split();
+                List<Tire> tiresList = new List<Tire>();
+
+                for (int i = 0; i < tiresData.Length; i+=2)
+                {
+                    int year = int.Parse(tiresData[i]);
+                    double pressure = double.Parse(tiresData[i + 1]);
+                    Tire tire = new Tire(year, pressure);
+                    tiresList.Add(tire);
+                }
+
+                tires.Add(tiresList.ToArray());
+
+                command = Console.ReadLine();
+            }
+
+            List<Engine> engines = new List<Engine>();
+
+            command = Console.ReadLine();
+            while (command != "Engines done")
+            {
+                string[] enginesData = command.Split();
+                int horsePower = int.Parse(enginesData[0]);
+                double cubicCapacity = double.Parse(enginesData[1]);
+                Engine engine = new Engine(horsePower, cubicCapacity);
+                engines.Add(engine);
+
+                command = Console.ReadLine();
+            }
+
+            List<Car> cars = new List<Car>();
+
+            command = Console.ReadLine();
+            while (command != "Show special")
+            {
+                string[] carData = command.Split();
+                string make = carData[0];
+                string model = carData[1];
+                int year = int.Parse(carData[2]);
+                double fuelQuantity = double.Parse(carData[3]);
+                double fuelConsumption = double.Parse(carData[4]);
+                int engineIndex = int.Parse(carData[5]);
+                int tiresIndex = int.Parse(carData[6]);
+
+                Car car = new Car(make, model, year, fuelQuantity, fuelConsumption, engines[engineIndex], tires[tiresIndex]);
+                cars.Add(car);
+
+                command = Console.ReadLine();
+            }
+
+            Func<Car, bool> filter = c => c.Year >= 2017 && c.Engine.HorsePower > 330 && c.Tires.Sum(t => t.Pressure) >= 9 && c.Tires.Sum(t => t.Pressure) <= 10;
+
+            List<Car> specialCars = new List<Car>();
+
+            foreach (Car car in cars.Where(filter))
+            {
+                car.Drive(20);
+                specialCars.Add(car);
+            }
+            Console.WriteLine(string.Join("", specialCars));
+        }
+    }
+}
