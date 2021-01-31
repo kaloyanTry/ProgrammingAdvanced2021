5cf6111e36c68c5acd88ff106120861583094ccc
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-31 09:11:35 +0200

    Add files via upload

---
+++ DifiningClasses/OldestFamilyMemeber/Family.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        public Family()
+        {
+            Members = new List<Person>();
+        }
+        public List<Person> Members { get; set; }
+
+        public void AddMember(Person member)
+        {
+            Members.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            return Members.OrderByDescending(m => m.Age).FirstOrDefault();
+        }
+    }
+}
---
+++ DifiningClasses/OldestFamilyMemeber/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Family family = new Family();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] personData = Console.ReadLine().Split();
+                string name = personData[0];
+                int age = int.Parse(personData[1]);
+
+                Person person = new Person(name, age);
+                family.AddMember(person);
+            }
+
+            Console.WriteLine(family.GetOldestMember().ToString());
+        }
+    }
+}
