b79734df7ca8d14afa9b966ec74a67a89c2528a7
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-31 09:14:29 +0200

    Add files via upload

---
+++ DifiningClasses/DateModifier/DateModifier.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace DateModifier
+{
+    public class DateModifier
+    {
+        private DateTime firstDate;
+        private DateTime secondDate;
+
+
+        public double CalculateDifferenceBetweenTwoDates(string firstDate, string secondDate)
+        {
+            string[] firstData = firstDate.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+            string[] secondData = secondDate.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            this.firstDate = new DateTime(int.Parse(firstData[0]), int.Parse(firstData[1]), int.Parse(firstData[2]));
+            this.secondDate = new DateTime(int.Parse(secondData[0]), int.Parse(secondData[1]), int.Parse(secondData[2]));
+
+            return Math.Abs((this.secondDate - this.firstDate).TotalDays);
+        }
+    }
+}
---
+++ DifiningClasses/DateModifier/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DateModifier
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            DateModifier modifier = new DateModifier();
+
+            string firstInput = Console.ReadLine();
+            string secondInput = Console.ReadLine();
+
+            Console.WriteLine(modifier.CalculateDifferenceBetweenTwoDates(firstInput, secondInput));
+        }
+    }
+}
