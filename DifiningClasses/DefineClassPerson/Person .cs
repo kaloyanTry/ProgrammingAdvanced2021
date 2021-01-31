c8061199679f373118d06ca71552dd1d3ac937e5
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-31 08:26:24 +0200

    Add files via upload

---
+++ DifiningClasses/DefineClassPerson/Person .cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        public Person(string name, int age)
+        {
+            name = "Pesho";
+            age = 20;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+    }
+}
---
+++ DifiningClasses/DefineClassPerson/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            Person person = new Person(name, age);
+
+        }
+    }
+}
