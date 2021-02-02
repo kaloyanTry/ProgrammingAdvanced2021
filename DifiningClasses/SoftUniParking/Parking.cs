8c5248fe3c6c04f8d268280f16dd5bce03259ac3
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-02-02 15:15:32 +0200

    SoftUniParking exam problem

---
+++ DifiningClasses/SoftUniParking/Parking.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace SoftUniParking
+{
+    public class Parking
+    {
+        private List<Car> cars;
+        private int capacity;
+
+        public Parking(int capacity)
+        {
+            this.capacity = capacity;
+            cars = new List<Car>();
+        }
+
+        public int Count => cars.Count;
+
+        public string AddCar(Car car)
+        {
+            if (cars.Any(c => c.RegistrationNumber == car.RegistrationNumber))
+            {
+                return "Car with that registration number, already exists!";
+            }
+            if (cars.Count >= capacity)
+            {
+                return "Parking is full!";
+            }
+
+            cars.Add(car);
+            return $"Successfully added new car {car.Make} {car.RegistrationNumber}";
+        }
+
+        public string RemoveCar(string registrationNumber)
+        {
+            Car car = cars.FirstOrDefault(c => c.RegistrationNumber == registrationNumber);
+
+            if (car == null)
+            {
+                return "Car with that registration number, doesn't exist!";
+            }
+            else
+            {
+                cars.Remove(car);
+                return $"Successfully removed {registrationNumber}";
+            }
+        }
+
+        public Car GetCar(string registrationNumber) => cars.FirstOrDefault(c => c.RegistrationNumber == registrationNumber);
+
+
+        public void RemoveSetOfRegistrationNumber (List<string> registrationNumbers)
+        {
+            foreach (var currentNumber in registrationNumbers)
+            {
+                cars.RemoveAll(c => c.RegistrationNumber == currentNumber);
+            }
+        }
+    }
+}
