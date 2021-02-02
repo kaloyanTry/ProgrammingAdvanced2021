9ecb3f59d64d17e7a76e77f5309084229271ed53
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-02-02 21:38:39 +0200

    Add files via upload

---
+++ DifiningClasses/PokemonTrainer/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace PokemonTrainer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Trainer> trainers = new Dictionary<string, Trainer>();
+
+            string line = Console.ReadLine();
+
+            while (line != "Tournament")
+            {
+                string[] tokens = line.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                if (!trainers.ContainsKey(tokens[0]))
+                {
+                    trainers[tokens[0]] = new Trainer(tokens[0]);
+                }
+
+                trainers[tokens[0]].Pokemon.Add(new Pokemon(tokens[1], tokens[2], int.Parse(tokens[3])));
+
+                line = Console.ReadLine();
+            }
+
+            line = Console.ReadLine();
+            while (line != "End")
+            {
+                foreach (var trainer in trainers)
+                {
+                    trainer.Value.CheckPokemonToBeAnElement(line);
+                }
+                line = Console.ReadLine();
+            }
+
+            foreach (var kvp in trainers.OrderByDescending(x => x.Value.Badges))
+            {
+                Console.WriteLine(kvp.Value);
+            }
+        }
+    }
+}
---
+++ DifiningClasses/PokemonTrainer/Trainer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace PokemonTrainer
+{
+    public class Trainer
+    {
+        public Trainer(string name)
+        {
+            Name = name;
+            Badges = 0;
+            Pokemon = new List<Pokemon>();
+        }
+        public string Name { get; set; }
+        public int Badges { get; set; }
+        public List<Pokemon> Pokemon { get; set; }
+
+
+        public void CheckPokemonToBeAnElement(string element)
+        {
+            if (Pokemon.Where(x => x.Element == element).ToList().Count >= 1)
+            {
+                Badges++;
+            }
+            else
+            {
+                for (int i = 0; i < Pokemon.Count; i++)
+                {
+                    Pokemon[i].Health -= 10;
+                    if (Pokemon[i].Health <= 0)
+                    {
+                        Pokemon.RemoveAt(i);
+                        i--;
+                    }
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {Badges} {Pokemon.Count}";
+        }
+    }
+}
