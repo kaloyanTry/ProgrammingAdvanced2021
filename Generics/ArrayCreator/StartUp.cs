28178d1dc202ce49caa4f1805f66129b94905439
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-02-05 22:04:04 +0200

    Add files via upload

---
+++ Generics/ArrayCreator/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericArrayCreator
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            List<string>[] array = ArrayCreator.Create(10, new List<string>() { "dimi", "best" });
+
+            foreach (var item in array)
+            {
+                foreach (var name in item)
+                {
+                    Console.WriteLine(name);
+                }
+            }       
+        }
+    }
+}
