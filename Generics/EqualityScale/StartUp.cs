cbdfe469827bc24fd98ab2d1ef155e50ddc70f21
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-02-05 22:03:03 +0200

    Add files via upload

---
+++ Generics/EqualityScale/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            EqualityScale<int> scale = new EqualityScale<int>(6, 6);
+            Console.WriteLine(scale.AreEqual());
+        }
+    }
+}
