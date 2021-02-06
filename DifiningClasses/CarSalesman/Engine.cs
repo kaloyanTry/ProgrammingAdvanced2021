f686e2611842c1ae5826b0e9cdfc9ec068cf7f61
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-02-06 18:18:11 +0200

    Update Engine.cs

--- DifiningClasses/CarSalesman/Engine.cs
+++ DifiningClasses/CarSalesman/Engine.cs
@@ -1,19 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace CarSalesman
+namespace CarSalesmanExercise
 {
     public class Engine
     {
-        public string Model { get; set; }
-
-        public string Power { get; set; }
-
-        public string Displacement { get; set; }
-
-        public string Efficiency { get; set; }
-
         public Engine(string model, string power, string displacement = "n/a", string efficiency = "n/a")
         {
             Model = model;
@@ -21,5 +13,10 @@ namespace CarSalesman
             Displacement = displacement;
             Efficiency = efficiency;
         }
+
+        public string Model { get; set; }
+        public string Power { get; set; }
+        public string Displacement { get; set; }
+        public string Efficiency { get; set; }
     }
 }
