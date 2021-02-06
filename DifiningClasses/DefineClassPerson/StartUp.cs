73dbc5fabd02318a66a0ba42cbda39708be5141e
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-02-06 08:02:08 +0200

    Update StartUp.cs

--- DifiningClasses/DefineClassPerson/StartUp.cs
+++ DifiningClasses/DefineClassPerson/StartUp.cs
@@ -6,11 +6,19 @@ namespace DefiningClasses
     {
         static void Main(string[] args)
         {
-            string name = Console.ReadLine();
-            int age = int.Parse(Console.ReadLine());
+            Person personOne = new Person
+            {
+                Name = "Pesho",
+                Age = 20
+            };
 
-            Person person = new Person(name, age);
+            Person personTwo = new Person();
+            personTwo.Name = "Gosho";
+            personTwo.Age = 18;
 
+            Person personThree = new Person();
+            personThree.Name = "Stamat";
+            personThree.Age = 43;
         }
     }
 }
