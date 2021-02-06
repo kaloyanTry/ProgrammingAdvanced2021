0be0443de98513559f4eed2f3e82bf79ea2eab36
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-02-06 08:03:07 +0200

    Update Person .cs

--- DifiningClasses/DefineClassPerson/Person .cs
+++ DifiningClasses/DefineClassPerson/Person .cs
@@ -6,14 +6,7 @@ namespace DefiningClasses
 {
     public class Person
     {
-        public Person(string name, int age)
-        {
-            name = "Pesho";
-            age = 20;
-        }
-
         public string Name { get; set; }
         public int Age { get; set; }
-
     }
 }
