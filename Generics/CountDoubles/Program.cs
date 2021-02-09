c746a3f02bcc68e690e1cd6ab872726698ac7043
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-02-09 18:20:51 +0200

    Add files via upload

---
+++ Generics/CountDoubles/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ConsoleGenerics
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Box<double>> boxes = new List<Box<double>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                double input = double.Parse(Console.ReadLine());
+                Box<double> box = new Box<double>(input);
+
+                boxes.Add(box);
+            }
+
+            double value = double.Parse(Console.ReadLine());
+            Box<double> comparableBox = new Box<double>(value);
+
+            int count = GetGreaterCount(boxes, comparableBox);
+
+            Console.WriteLine(count);
+        }
+
+        private static int GetGreaterCount<T>(List<Box<T>> boxes, Box<T> box) where T: IComparable
+        {
+            int count = 0;
+            foreach (Box<T> item in boxes)
+            {
+                if (item.Value.CompareTo(box.Value) > 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        private static void SwapIndexes<T>(List<Box<T>> boxes, int firstIndex, int secondIndex) where T: IComparable
+        {
+            Box<T> tempValue = boxes[firstIndex];
+            boxes[firstIndex] = boxes[secondIndex];
+            boxes[secondIndex] = tempValue;        
+        }
+    }
+}
