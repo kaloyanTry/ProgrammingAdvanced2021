69df2e5cd7339f27ee9f196c5e0c6765329e30ca
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-02-09 21:49:25 +0200

    Add files via upload

---
+++ InteratorsComparators/LibraryIterator/Library.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            private List<Book> books;
+            private int index;
+
+            public LibraryIterator(List<Book> books)
+            {
+                this.books = books;
+                index = -1;
+            }
+
+            public bool MoveNext()
+            {
+                index++;
+
+                if (index < books.Count)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+
+            public void Reset()
+            {
+                index = -1;
+            }
+
+            public Book Current
+            {
+                get => books[index];
+            }
+
+            object IEnumerator.Current => Current;
+
+            public void Dispose()
+            {
+
+            }
+        }
+
+        private List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = new List<Book>(books);
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            LibraryIterator iter = new LibraryIterator(books);
+
+            return iter;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
