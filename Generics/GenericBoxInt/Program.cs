7967f96a6f945a815107a9d21ae5807411d9d3c0
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-02-09 16:02:07 +0200

    Add files via upload

---
+++ Generics/GenericBoxInt/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleGenerics
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+
+                Box<int> box = new Box<int>(input);
+
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
