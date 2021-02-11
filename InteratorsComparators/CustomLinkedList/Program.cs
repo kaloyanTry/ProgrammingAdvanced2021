534be782e401f0f643637de4064fa745978b0d0b
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-02-11 21:53:47 +0200

    Add files via upload

---
+++ InteratorsComparators/CustomLinkedList/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CustomLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomLinkedList<int> list = new CustomLinkedList<int>();
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                var tokens = Console.ReadLine().Split();
+
+                string command = tokens[0];
+                int number = int.Parse(tokens[1]);
+
+                switch (command)
+                {
+                    case "Add":
+                        list.Add(number);
+                        break;
+                    case "Remove":
+                        list.Remove(number);
+                        break;
+                }
+            }
+
+            Console.WriteLine(list.Count);
+
+            foreach (int number in list)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
