d41f30d8d72c795dbe40388e98aea90c760dba6e
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-02-11 17:47:57 +0200

    100/100 judge but not finished Report() ???

---
+++ OldExams/2020.12.12.retakeExam/Problem3BakeryOpenning/Bakery.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BakeryOpenning
+{
+    public class Bakery
+    {
+        private List<Employee> data;
+
+        public Bakery(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            data = new List<Employee>(); 
+        }
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+
+        public int Count => data.Count;
+
+        public void Add(Employee employee)
+        {
+            data.Add(employee);
+        }
+
+        public bool Remove(string name)
+        {
+            Employee employee = data.FirstOrDefault(e => e.Name == name);
+            if (employee == null)
+            {
+                return false;
+            }
+            else
+            {
+                data.Remove(employee);
+                return true;
+            }
+        }
+        public Employee GetOldestEmployee()
+        {
+            return data.OrderByDescending(e => e.Age).FirstOrDefault();
+        }
+
+        public Employee GetEmployee(string name) => data.FirstOrDefault(e => e.Name == name);
+
+        public string Report()
+        {
+            return $"employees working at bakery {Name}:";
+        }
+    }
+}
