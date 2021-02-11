ad21a26fe37b7bccd26ffbeef49b26ff9d8e4e5b
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-02-11 21:58:14 +0200

    Add files via upload

---
+++ InteratorsComparators/ListyIterator/ListyIterator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Text;
+
+namespace ListyIterator
+{
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+        private List<T> collection;
+        private int currentIndex;
+
+        public ListyIterator(params T[] collection)
+        {
+            this.collection = new List<T>(collection);
+        }
+
+        public bool Move()
+        {
+            if (collection.Count - 1 > currentIndex)
+            {
+                currentIndex++;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool HasNext()
+        {
+            return currentIndex + 1 < collection.Count;
+        }
+
+        public void Print()
+        {
+            if (this.collection.Count == 0)
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+
+            Console.WriteLine(collection[currentIndex]);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < collection.Count; i++)
+            {
+                yield return collection[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+    }
+}
---
+++ InteratorsComparators/ListyIterator/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+
+namespace ListyIterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListyIterator<string> listyIterator = null;
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "END")
+                {
+                    break;
+                }
+
+                string[] data = command.Split();
+
+                switch (data[0])
+                {
+                    case "Create":
+                        listyIterator = new ListyIterator<string>(data.Skip(1).ToArray());
+                        break;
+
+                    case "Print":
+                        try
+                        {
+                            listyIterator.Print();
+                        }
+                        catch (InvalidOperationException ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        break;
+
+                    case "PrintAll":
+                        string message = String.Empty;
+                        foreach (string element in listyIterator)
+                        {
+                            message += element + " ";
+                        }
+                        Console.WriteLine(message.TrimEnd());
+                        break;
+
+                    case "Move":
+                        Console.WriteLine(listyIterator.Move());
+                        break;
+
+                    case "HasNext":
+                        Console.WriteLine(listyIterator.HasNext());
+                        break;
+                }
+            }
+            
+        }
+    }
+}
