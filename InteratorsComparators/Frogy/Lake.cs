1b717716d9e1afa501d14e1f34961f362117e0a1
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-02-11 21:55:57 +0200

    Add files via upload

---
+++ InteratorsComparators/Frogy/Lake.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Frogy
+{
+    public class Lake : IEnumerable<int>
+    {
+        private int[] stones;
+
+        public Lake(int[] stones)
+        {
+            this.stones = stones;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < stones.Length; i += 2)
+            {
+                yield return stones[i];
+            }
+
+            int reversedIndex = stones.Length % 2 == 0 ? stones.Length - 1 : stones.Length - 2;
+            for (int i = reversedIndex; i > 0; i -= 2)
+            {
+                yield return stones[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+    }
+}
---
+++ InteratorsComparators/Frogy/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Frogy
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] stones = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            Lake lake = new Lake(stones);
+
+            Console.WriteLine(string.Join(", ", lake));
+        }
+    }
+}
