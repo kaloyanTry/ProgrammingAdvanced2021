9f030158155780e63e5573f58461064ce9c5ac54
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-02-11 21:54:31 +0200

    Add files via upload

---
+++ InteratorsComparators/CustomStack/CustomStack.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class CustomStack<T> : IEnumerable<T>
+
+    {
+        private List<T> elements;
+        private int currentIndex = -1;
+
+        public CustomStack()
+        {
+            elements = new List<T>();
+        }
+
+        public void Push(params T[] items)
+        {
+            foreach (var item in items)
+            {
+                elements.Insert(++currentIndex, item);
+            }
+        }
+
+        public void Pop()
+        {
+            if (currentIndex < 0)
+            {
+                throw new InvalidOperationException("No elements");
+            }
+
+            currentIndex--;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = currentIndex; i >= 0; i--)
+            {
+                yield return elements[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+    }
+}
---
+++ InteratorsComparators/CustomStack/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace CustomStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomStack<int> stack = new CustomStack<int>();
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "END")
+                {
+                    break;
+                }
+
+                string[] tokens = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                switch (tokens[0])
+                {
+                    case "Push":
+                        int[] elements = tokens.Skip(1).Select(i => i.Split(',').First()).Select(int.Parse).ToArray();
+                        stack.Push(elements);
+                        break;
+
+                    case "Pop":
+                        try
+                        {
+                            stack.Pop();
+                        }
+                        catch (InvalidOperationException ioe)
+                        {
+                            Console.WriteLine(ioe.Message);
+                        }
+                        break;
+                }
+            }
+
+            foreach (int number in stack)
+            {
+                Console.WriteLine(number);
+            }
+
+            foreach (int number in stack)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
