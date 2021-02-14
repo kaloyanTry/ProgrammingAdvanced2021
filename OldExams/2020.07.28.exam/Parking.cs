9beba23d4fccadeaedd710d53465035a7ddd8bfc
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-02-14 12:14:38 +0200

    100/100 judge

---
+++ OldExams/2020.07.28.exam/Parking.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Parking
+{
+    public class Parking
+    {
+        private List<Car> data;
+
+        public Parking(string type, int capacity)
+        {
+            Type = type;
+            Capacity = capacity;
+            data = new List<Car>();
+        }
+
+        public string Type { get; set; }
+        public int Capacity { get; set; }
+
+        public int Count { get { return data.Count; } }
+
+        public void Add(Car car)
+        {
+            if (Capacity > data.Count)
+            {
+                data.Add(car);
+            }
+        }
+
+        public bool Remove(string manufacturer, string model)
+        {
+            Car car = data.FirstOrDefault(c => c.Manufacturer == manufacturer && c.Model == model);
+
+            if (car != null)
+            {
+                data.Remove(car);
+                return true;
+            }
+            return false;
+        }
+
+        public Car GetLatestCar()
+        {
+            Car car = data.OrderByDescending(c => c.Year).FirstOrDefault();
+
+             return car;
+
+        }
+
+        public Car GetCar(string manufacturer, string model)
+        {
+            Car car = data.FirstOrDefault(c => c.Manufacturer == manufacturer && c.Model == model);
+
+            return car;
+        }
+
+        public string GetStatistics()
+        {
+            StringBuilder result = new StringBuilder();
+
+            result.AppendLine($"The cars are parked in {Type}:");
+
+            foreach (var car in data)
+            {
+                result.AppendLine(car.ToString());
+            }
+
+            return result.ToString().Trim();
+        }
+    }
+}
