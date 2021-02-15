63eb617c49c93d06e4e42a92246186617629bd4c
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-02-15 16:38:09 +0200

    Add files via upload

---
+++ OldExams/2020.02.22.exam/LootBox.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace LootBox
+{
+    class LootBox
+    {
+        static void Main(string[] args)
+        {
+            int[] firstInput = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int[] secondInput = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            Queue<int> firstBoxQueue = new Queue<int>(firstInput.Length);
+            Stack<int> secondBoxStack = new Stack<int>(secondInput.Length);
+
+            for (int i = 0; i < firstInput.Length; i++)
+            {
+                firstBoxQueue.Enqueue(firstInput[i]);
+            }
+            for (int i = 0; i < secondInput.Length; i++)
+            {
+                secondBoxStack.Push(secondInput[i]);
+            }
+
+            int sumClaimedItems = 0;
+
+            while (firstBoxQueue.Count > 0 && secondBoxStack.Count > 0)
+            {
+                int currentFirst = firstBoxQueue.Peek();
+                int currentSecond = secondBoxStack.Peek();
+                int currentsSum = currentFirst + currentSecond;
+
+                if (currentsSum % 2 == 0)
+                {
+                    sumClaimedItems += currentsSum;
+                    firstBoxQueue.Dequeue();
+                    secondBoxStack.Pop();
+                }
+                else
+                {
+                    int temp = currentSecond;
+                    secondBoxStack.Pop();
+                    firstBoxQueue.Enqueue(temp);
+                }
+            }
+
+            if (firstBoxQueue.Count == 0 && secondBoxStack.Count > 0)
+            {
+                Console.WriteLine("First lootbox is empty");
+            }
+            else
+            {
+                Console.WriteLine("Second lootbox is empty");
+            }
+
+            if (sumClaimedItems >= 100)
+            {
+                Console.WriteLine($"Your loot was epic! Value: {sumClaimedItems}");
+
+            }
+            else
+            {
+                Console.WriteLine($"Your loot was poor... Value: {sumClaimedItems}");
+            }
+        }
+    }
+}
---
+++ OldExams/2020.02.22.exam/ReVolt.cs
@@ -0,0 +1,159 @@
+﻿using System;
+
+namespace Revolt
+{
+    class ReVolt
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[n, n];
+
+            int playerRow = -1;
+            int playerCol = -1;            
+
+            for (int row = 0; row < n; row++)
+            {
+                string inputRow = Console.ReadLine();
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = inputRow[col];
+
+                    if (matrix[row, col] == 'f')
+                    {
+                        playerRow = row;
+                        playerCol = col;
+                    }
+                }
+            }
+
+
+            for (int i = 0; i < c; i++)
+            {
+                string command = Console.ReadLine();
+                matrix[playerRow, playerCol] = '-';
+
+                playerRow = MoveRow(playerRow, command);
+                playerCol = MoveCol(playerCol, command);
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    for (int col = 0; col < matrix.GetLength(1); col++)
+                    {
+
+                        if (playerRow == matrix.GetLength(0))
+                        {
+                            playerRow = 0;
+                        }
+                        if (playerRow < 0)
+                        {
+                            playerRow = matrix.GetLength(0) - 1;
+                        }
+                        if (playerCol == matrix.GetLength(1) - 1)
+                        {
+                            playerCol = 0;
+                        }
+                        if (playerCol < 0)
+                        {
+                            playerCol = matrix.GetLength(1) - 1;
+                        }
+
+                        if (matrix[playerRow, playerCol] == 'B')
+                        {
+                            playerRow = MoveRow(playerRow, command);
+                            playerCol = MoveCol(playerCol, command);
+                            matrix[playerRow, playerCol] = 'f';
+                        }
+
+                        if (matrix[playerRow, playerCol] == 'T')
+                        {
+                            playerRow = MoveBackRow(playerRow, command);
+                            playerCol = MoveBackCol(playerCol, command);
+                            matrix[playerRow, playerCol] = 'f';
+                        }
+
+                        if (matrix[playerRow, playerCol] == 'F')
+                        {
+                            Console.WriteLine("Player won!");
+                            matrix[playerRow, playerCol] = 'f';
+                            PrintMatrix(matrix);
+                            return;
+                        }
+
+                        matrix[playerRow, playerCol] = 'f';
+                    }
+                }
+            }
+
+            Console.WriteLine("Player lost!");
+
+            PrintMatrix(matrix);
+        }
+        public static int MoveRow(int row, string movement)
+        {
+            if (movement == "up")
+            {
+                return row - 1;
+            }
+            if (movement == "down")
+            {
+                return row + 1;
+            }
+
+            return row;
+        }
+
+        public static int MoveCol(int col, string movement)
+        {
+            if (movement == "left")
+            {
+                return col - 1;
+            }
+            if (movement == "right")
+            {
+                return col + 1;
+            }
+
+            return col;
+        }
+        public static int MoveBackRow(int row, string movement)
+        {
+            if (movement == "up")
+            {
+                return row + 1;
+            }
+            if (movement == "down")
+            {
+                return row - 1;
+            }
+
+            return row;
+        }
+
+        public static int MoveBackCol(int col, string movement)
+        {
+            if (movement == "left")
+            {
+                return col + 1;
+            }
+            if (movement == "right")
+            {
+                return col - 1;
+            }
+
+            return col;
+        }
+        private static void PrintMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
