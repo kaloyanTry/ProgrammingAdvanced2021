c661b4455cda14cce7b755c3d5bb998e59c748a9
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-02-18 20:44:46 +0200

    93/100 judge
    9. test is not working

---
+++ OldExams/2020.10.25.exam/Classroom.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ClassroomProject
+{
+    public class Classroom
+    {
+        private List<Student> students;
+
+        public Classroom(int capacity)
+        {
+            Capacity = capacity;
+            students = new List<Student>();             
+        }
+        public int Capacity { get; set; }
+        public int Count { get { return students.Count; } }
+
+        public string RegisterStudent(Student student)
+        {          
+            if (Capacity > Count)
+            {
+                students.Add(student);
+                return $"Added student {student.FirstName} {student.LastName}";
+            }
+            else
+            {
+                return "No seats in the classroom";
+            }
+        }
+
+        public string DismissStudent(string firstName, string lastName)
+        {
+            Student student = students.FirstOrDefault(s => s.FirstName == firstName && s.LastName == lastName);
+            if (student != null)
+            {
+                students.Remove(student);
+                return $"Dismissed student {student.FirstName} {student.LastName}";
+            }
+            return "Student not found";
+        }
+
+        public string GetSubjectInfo(string subject)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Subject: {subject}");
+
+            if (students.Any(s => s.Subject == subject))
+            {
+                sb.AppendLine("Students:");
+                foreach (var stu in students.Where(s => s.Subject == subject))
+                {
+                     sb.AppendLine($"{stu.FirstName} {stu.LastName}");
+                }
+            }
+            else
+            {
+                sb.Append("No students enrolled for the subject");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public int GetStudentsCount()
+        {
+            return Count;
+        }
+
+        public Student GetStudent(string firstName, string lastName)
+        {
+            Student student = students.FirstOrDefault(s => s.FirstName == firstName && s.LastName == lastName);
+
+            return student;
+        }
+    }
+}
