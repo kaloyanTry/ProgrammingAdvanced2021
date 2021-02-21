6c61e4f74f1f75f7fac5d6dd7499b507c8184732
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-02-21 20:52:11 +0200

    Add files via upload

---
+++ OldExams/20.02.2021.exam/The Race/Race.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace TheRace
+{
+    public class Race
+    {
+        private List<Racer> data;
+        public Race(string  name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            data = new List<Racer>();
+        }
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+
+        public int Count => data.Count;
+
+        public void Add(Racer Racer)
+        {
+            if (Capacity > data.Count)
+            {
+                data.Add(Racer);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            Racer racer = data.FirstOrDefault(r => r.Name == name);
+
+            if (racer != null)
+            {
+                data.Remove(racer);
+                return true;
+            }
+            return false;
+        }
+
+        public Racer GetOldestRacer()
+        {
+            return data.OrderByDescending(r => r.Age).FirstOrDefault();
+        }
+
+        public Racer GetRacer(string name)
+        {
+            return data.FirstOrDefault(r => r.Name == name);
+        }
+
+        public Racer GetFastestRacer()
+        {
+            return data.OrderByDescending(r => r.Car.Speed).FirstOrDefault();
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Racers participating at {Name}:");
+
+            foreach (var racer in data)
+            {
+                sb.AppendLine(racer.ToString());
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
