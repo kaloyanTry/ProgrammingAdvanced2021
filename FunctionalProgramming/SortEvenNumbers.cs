a40d217f8f4c86fb2b323934e155ea240bffd562
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-02-22 20:59:14 +0200

    100/100

---
+++ FunctionalProgramming/SortEvenNumbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace EvenNumbers
+{
+    class EvenNumbers
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(", ").Select(int.Parse).ToList();
+
+            int[] evenNums = numbers.Where(n => n % 2 == 0).OrderBy(n => n).ToArray();
+
+            Console.WriteLine(string.Join(", ", evenNums));
+        }
+    }
+}
