f3ef2f584a234aaa5a84cf6f35b7b7b6ca34f6fb
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-02-23 15:24:27 +0200

    racursion factorial number

---
+++ AlgorithmsBasic/FactorialN.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FactoialN
+{
+    class FactorialN
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please write a number: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.Write($"The factorial of the number {n}! = ");
+            Console.WriteLine(Factorial(n));
+        }
+
+        private static long Factorial(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            return n * Factorial(n - 1);
+        }
+    }
+}
