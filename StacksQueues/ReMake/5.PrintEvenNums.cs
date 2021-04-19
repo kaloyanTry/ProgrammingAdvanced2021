df508595d3dd88408b72d319bdd4ebec61d00e91
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-04-19 20:46:00 +0300

    Add files via upload

---
+++ StacksQueues/ReMake/5.PrintEvenNums.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5.PrintEvenNumbersQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> queueEvens = new Queue<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int curNum = input[i];
+
+                if (curNum % 2 == 0)
+                {
+                    queueEvens.Enqueue(curNum);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", queueEvens));
+        }
+    }
+}
