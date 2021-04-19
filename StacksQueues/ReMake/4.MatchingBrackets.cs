7e02c5921c8a65266e0226fb38f20a32bf34fe9a
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-04-19 17:54:42 +0300

    Add files via upload

---
+++ StacksQueues/ReMake/4.MatchingBrackets.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4.MatchingBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<int> stackBrackets = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    stackBrackets.Push(i);
+                }
+
+                if (input[i] == ')')
+                {
+                    int startIndex = stackBrackets.Pop();
+                    Console.WriteLine(input.Substring(startIndex, i - startIndex + 1));
+                }
+            }
+        }
+    }
+}
