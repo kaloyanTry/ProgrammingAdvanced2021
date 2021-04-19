5b45660602154c11080ca1734abb002e159d6135
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-04-19 17:31:07 +0300

    Add files via upload

---
+++ StacksQueues/ReMake/3.SimpleCalculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3.SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split().ToArray();
+
+            Stack<string> stackCalc = new Stack<string>();
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                stackCalc.Push(input[i]);
+            }
+
+            while (stackCalc.Count > 1)
+            {
+                int firstNum = int.Parse(stackCalc.Pop());
+                char signCalc = char.Parse(stackCalc.Pop());
+                int secondNum = int.Parse(stackCalc.Pop());
+
+                if (signCalc == '+')
+                {
+                    stackCalc.Push((firstNum + secondNum).ToString());
+                }
+
+                if (signCalc == '-')
+                {
+                    stackCalc.Push((firstNum - secondNum).ToString());
+                }
+            }
+
+            Console.WriteLine(stackCalc.Pop());
+        }
+    }
+}
