53ddbb47feef4cef0ca1c011997fe6e1d4330060
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-04-19 16:43:28 +0300

    Add files via upload

---
+++ StacksQueues/ReMake/1.ReverseString.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1.ReverseStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> stackChars = new Stack<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                stackChars.Push(input[i]);
+            }
+
+            while (stackChars.Count != 0)
+            {
+                Console.Write(stackChars.Pop());
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ StacksQueues/ReMake/2.StackSum.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2.StackSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputNums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sum = 0;
+            Stack<int> stackSum = new Stack<int>(inputNums);
+
+            foreach (var num in inputNums)
+            {
+                stackSum.Push(num);
+            }
+
+            string cmd = Console.ReadLine().ToLower();
+            while (cmd != "end")
+            {
+                string[] inputString = cmd.Split();
+
+                string command = inputString[0];
+                if (command == "add")
+                {
+                    int firstNum = int.Parse(inputString[1]);
+                    int secondNum = int.Parse(inputString[2]);
+
+                    stackSum.Push(firstNum);
+                    stackSum.Push(secondNum);
+                }
+
+                if (command == "remove")
+                {
+                    int remNum = int.Parse(inputString[1]);
+
+                    if (stackSum.Count >= remNum)
+                    {
+                        while(remNum > 0)
+                        {
+                            stackSum.Pop();
+                            remNum--;
+                        }
+                    }
+                }
+
+                cmd = Console.ReadLine().ToLower();
+            }
+
+            sum = stackSum.Sum();
+
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
