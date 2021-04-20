28bb688aab260bf57359b4066377b71ca16228d2
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-04-20 16:49:46 +0300

    100/100

---
+++ StacksQueues/ReMake/8.TrafficJam.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TrafficJam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Queue<string> queueCars = new Queue<string>();
+
+            int countCars = 0;
+
+            string input = Console.ReadLine();
+            while (input != "end")
+            {
+                if (input == "green")
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        if (queueCars.Count > 0)
+                        {
+                            Console.WriteLine($"{queueCars.Dequeue()} passed!");
+                            countCars++;
+                        }
+                    }
+                }
+                else
+                {
+                    queueCars.Enqueue(input);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{countCars} cars passed the crossroads.");
+        }
+    }
+}
