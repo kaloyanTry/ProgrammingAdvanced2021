0175461f5a79e260ec6796fd2cacd4367857d757
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-04-20 19:46:39 +0300

    Add files via upload

---
+++ StacksQueues/ReMake/Ex3.MaxMinElement.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ex3.MaxMinElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Stack<int> stackQueue = new Stack<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] inputNums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                int firstNum = inputNums[0];
+
+                if (firstNum == 1)
+                {
+                    int secondNum = inputNums[1];
+                    stackQueue.Push(secondNum);
+                }
+                else if (firstNum == 2)
+                {
+                    if (stackQueue.Count > 0)
+                    {
+                        stackQueue.Pop();
+                    }                
+                }
+                else if (firstNum == 3)
+                {
+                    if (stackQueue.Count > 0)
+                    {
+                        Console.WriteLine(stackQueue.Max());
+                    }                   
+                }
+                else if (firstNum == 4)
+                {
+                    if (stackQueue.Count > 0)
+                    {
+                        Console.WriteLine(stackQueue.Min());
+                    }      
+                }
+            }
+
+            if (stackQueue.Count > 0)
+            {
+                Console.WriteLine(string.Join(", ", stackQueue));
+            }           
+        }
+    }
+}
