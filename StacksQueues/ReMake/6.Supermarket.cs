ae7a3e55b6afcf22bd51746e9645583608f002aa
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-04-20 15:29:18 +0300

    100/100

---
+++ StacksQueues/ReMake/6.Supermarket.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _6.Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Queue<string> queueNames = new Queue<string>();
+
+
+            while (input != "End")
+            {       
+                if (input == "Paid")
+                {
+                    while (queueNames.Count > 0)
+                    {
+                        Console.WriteLine(queueNames.Dequeue());
+                    }
+                }
+                else
+                {
+                    queueNames.Enqueue(input);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{queueNames.Count} people remaining.");
+        }
+    }
+}
