4492d499453596c3626ee1030da55ac9e2ee77c1
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-04-20 21:40:59 +0300

    100/100

---
+++ StacksQueues/ReMake/Ex4.FastFood.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ex4.FastFood
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int foodAll = int.Parse(Console.ReadLine());
+
+            Queue<int> queueOrders = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+
+            Console.WriteLine(queueOrders.Max());
+
+            while (queueOrders.Count > 0)
+            {
+                int order = queueOrders.Peek();
+                if (foodAll >= order)
+                {
+                    foodAll -= order;
+                    queueOrders.Dequeue();
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (queueOrders.Count == 0)
+            {
+                Console.WriteLine("Orders complete");
+            }
+            else
+            {
+                Console.WriteLine("Orders left: " + string.Join(" ", queueOrders));
+            }
+        }
+    }
+}
