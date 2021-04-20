355a866424b8658911bcb09626d437f4e0ced49a
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-04-20 15:41:42 +0300

    100/100

---
+++ StacksQueues/ReMake/7.HotPotato.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputNames = Console.ReadLine().Split();
+            int n = int.Parse(Console.ReadLine());
+
+            Queue<string> queueNames = new Queue<string>(inputNames);
+
+            while (queueNames.Count > 1)
+            {
+                for (int i = 1; i < n; i++)
+                {
+                    queueNames.Enqueue(queueNames.Dequeue());
+                }
+
+                Console.WriteLine($"Removed {queueNames.Dequeue()}");
+            }
+
+            Console.WriteLine("Last is " + queueNames.Dequeue());
+        }
+    }
+}
