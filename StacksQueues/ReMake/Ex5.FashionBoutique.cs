a292ffccfed7010430cc07fa61efd7c3c42d6df4
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-04-21 14:36:52 +0300

    Add files via upload

---
+++ StacksQueues/ReMake/Ex5.FashionBoutique.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ex5.FashionBoutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stackClothes = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+            int capacityRack = int.Parse(Console.ReadLine());
+
+            int countRacks = 1;
+            int rack = 0;
+
+            while (stackClothes.Any())
+            {
+                int pieceCloth = stackClothes.Pop();
+                rack += pieceCloth;
+
+                if (rack > capacityRack)
+                {
+                    countRacks++;
+                    rack = pieceCloth;
+                }
+            }
+
+            Console.WriteLine(countRacks);
+        }
+    }
+}
