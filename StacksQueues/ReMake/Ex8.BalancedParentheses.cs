af9dd99f629053654da543a9c396a4f8bb2fb34d
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-04-21 19:02:16 +0300

    100/100

---
+++ StacksQueues/ReMake/Ex8.BalancedParentheses.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ex8.BalancedParentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<char> bracketsStack = new Stack<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '{')
+                {
+                    bracketsStack.Push(input[i]);
+                }
+
+                if (input[i] == '(')
+                {
+                    bracketsStack.Push(input[i]);
+                }
+
+                if (input[i] == '[')
+                {
+                    bracketsStack.Push(input[i]);
+                }
+
+                if (input[i] == '}')
+                {
+                    if (bracketsStack.Any() && bracketsStack.Peek() == '{')
+                    {
+                        bracketsStack.Pop();
+                    }
+                    else
+                    {
+                        Console.WriteLine("NO");
+                        return;
+                    }
+                }
+
+                if (input[i] == ')')
+                {
+                    if (bracketsStack.Any() && bracketsStack.Peek() == '(')
+                    {
+                        bracketsStack.Pop();
+                    }
+                    else
+                    {
+                        Console.WriteLine("NO");
+                        return;
+                    }
+                }
+
+                if (input[i] == ']')
+                {
+                    if (bracketsStack.Any() && bracketsStack.Peek() == '[')
+                    {
+                        bracketsStack.Pop();
+                    }
+                    else
+                    {
+                        Console.WriteLine("NO");
+                        return;
+                    }
+                }
+            }
+
+            if (bracketsStack.Any())
+            {
+                Console.WriteLine("NO");
+            }
+            else
+            {
+                Console.WriteLine("YES");
+            }
+        }
+    }
+}
