d47167bf69c8453d1f210db0b4999ed894763d34
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-04-23 20:55:44 +0300

    Add files via upload

--- StacksQueues/ReMake/Ex7.TruckTour.cs
+++ StacksQueues/ReMake/Ex7.TruckTour.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Ex7.TruckTour
 {
@@ -6,7 +8,56 @@ namespace Ex7.TruckTour
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int pumpsCount = int.Parse(Console.ReadLine());
+            int index = 0;
+            Queue<int> difference = new Queue<int>();
+
+            for (int i = 0; i < pumpsCount; i++)
+            {
+                int[] pumpProps = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+                difference.Enqueue(pumpProps[0] - pumpProps[1]);
+            }
+
+            while (true)
+            {
+                Queue<int> testQueue = new Queue<int>(difference);
+                int fuel = -1;
+
+                while (testQueue.Any())
+                {
+                    if (testQueue.Peek() > 0 && fuel == -1)
+                    {
+                        fuel = testQueue.Dequeue();
+                        difference.Enqueue(difference.Dequeue());
+                    }
+
+                    else if (testQueue.Peek() < 0 && fuel == -1)
+                    {
+                        testQueue.Enqueue(testQueue.Dequeue());
+                        difference.Enqueue(difference.Dequeue());
+                        index++;
+                    }
+
+                    else
+                    {
+                        fuel += testQueue.Dequeue();
+
+                        if (fuel < 0)
+                        {
+                            break;
+                        }
+                    }
+                }
+
+                if (fuel >= 0)
+                {
+                    Console.WriteLine(index);
+                    return;
+                }
+
+                index++;
+            }
         }
     }
 }
