eab6f47ee54e2c0f37375e33c7008cd70a6553f9
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-04-23 20:54:33 +0300

    100/100

---
+++ StacksQueues/ReMake/Ex9.SimpleTextEditor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex9.SimpleTextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Stack<string> stackText = new Stack<string>();
+            StringBuilder text = new StringBuilder();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] cmdInput = Console.ReadLine().Split();
+                string command = cmdInput[0];
+
+                if (command == "1")
+                {
+                    stackText.Push(text.ToString());
+                    text.Append(cmdInput[1]);
+                }
+                else if (command == "2")
+                {
+                    int index = int.Parse(cmdInput[1]);
+                    stackText.Push(text.ToString());
+                    text.Remove(text.Length - index, index);
+                }
+                else if (command == "3")
+                {
+                    int index = int.Parse(cmdInput[1]);
+                    Console.WriteLine(text[index - 1]);
+                }
+                else if (command == "4")
+                {
+                    if (stackText.Count > 0)
+                    {
+                        text.Clear();
+                        text.Append(stackText.Pop());
+                    }
+                    
+                }
+            }
+        }
+    }
+}
