a1daeda3b57f17083ecc9cef6fc4d1c99af25b38
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-04-25 19:21:48 +0300

    100/100

---
+++ Matrix/ReMake/Ex3.MaxSum3x3.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+
+namespace Ex3.MaxSum3x3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] size = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int mRow = size[0];
+            int mCol = size[1];
+
+            int[,] matrix = new int[mRow, mCol];
+            int maxSum = int.MinValue;
+            int maxRow = -1;
+            int maxCol = -1;
+
+            for (int row = 0; row < mRow; row++)
+            {
+                int[] data = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+                for (int col = 0; col < mCol; col++)
+                {
+                    matrix[row, col] = data[col];
+                }
+            }
+
+            for (int row = 0; row < matrix.GetLength(0) - 2; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 2; col++)
+                {
+                    int currentSum = matrix[row, col] + matrix[row, col + 1] + matrix[row, col + 2] + matrix[row + 1, col] + matrix[row + 1, col + 1] + matrix[row + 1, col + 2] + matrix[row + 2, col] + matrix[row + 2, col + 1] + matrix[row + 2, col + 2];
+                    if (currentSum > maxSum)
+                    {
+                        maxSum = currentSum;
+                        maxRow = row;
+                        maxCol = col;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Sum = {maxSum}");
+
+            for (int row = maxRow; row <= maxRow + 2; row++)
+            {
+                for (int col = maxCol; col <= maxCol + 2; col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
