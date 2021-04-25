f40584867c62aa733cae91ad02c3654ffa5cce24
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-04-25 08:11:31 +0300

    100/100

---
+++ Matrix/ReMake/3.PrimaryDiagonalSum.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace _3.PrimaryDiagonalSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+            int sumDiagonal = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                int[] data = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] += data[col];
+                }
+            }
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (row == col)
+                    {
+                        sumDiagonal += matrix[row, col];
+                    }
+                }
+            }
+
+            Console.WriteLine(sumDiagonal);
+        }
+    }
+}
