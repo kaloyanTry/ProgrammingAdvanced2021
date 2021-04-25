a71e253158a20ed24f156031e1448c5c74d0c7f7
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-04-25 08:53:16 +0300

    100/100

---
+++ Matrix/ReMake/4.SymbolInMatrix.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+
+namespace SymbolMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[n, n];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string input = Console.ReadLine();
+                char[] data = input.ToCharArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = data[col];
+                }
+            }
+
+            char symbol = char.Parse(Console.ReadLine());
+            int symbolRow = -1;
+            int symbolCol = -1;
+            bool isSymbol = false;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] == symbol)
+                    {
+                        isSymbol = true;
+                        symbolRow = row;
+                        symbolCol = col;
+                    }
+                }
+
+                if (isSymbol)
+                {
+                    break;
+                }
+            }
+
+            if (isSymbol)
+            {
+                Console.WriteLine($"({symbolRow}, {symbolCol})");
+            }
+            else
+            {
+                Console.WriteLine($"{symbol} does not occur in the matrix");
+            }
+        }
+    }
+}
