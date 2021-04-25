b0f5380338945c3ec919182546da29ef21818817
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-04-25 11:34:25 +0300

    100/100

---
+++ Matrix/ReMake/Ex01.DiagonalDifference.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace Ex01.DiagonalDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+
+            int firstSum = 0;
+            int secondSum = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                int[] data = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = data[col];
+                }
+            }
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (row == col)
+                    {
+                        firstSum += matrix[row, col];                    
+                    }
+
+                    if (col == n - row - 1)
+                    {
+                        secondSum += matrix[row, col];
+                    }
+                }
+            }
+
+            Console.WriteLine(Math.Abs(firstSum - secondSum));
+        }
+    }
+}
