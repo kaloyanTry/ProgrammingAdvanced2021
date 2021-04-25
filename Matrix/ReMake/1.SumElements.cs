373f09b0596f97dc671265bbb26d99d4894b6320
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-04-25 07:29:25 +0300

    100/100

---
+++ Matrix/ReMake/1.SumElements.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mSize = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            int mRows = mSize[0];
+            int mCols = mSize[1];
+            int[,] matrix = new int[mRows, mCols];
+
+            int sumElements = 0;
+
+            for (int row = 0; row < mRows; row++)
+            {
+                int[] rowData = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+                for (int col = 0; col < mCols; col++)
+                {
+                    matrix[row, col] = rowData[col];
+
+                    sumElements += matrix[row, col];
+                }
+            }
+
+            Console.WriteLine(matrix.GetLength(0));
+            Console.WriteLine(matrix.GetLength(1));
+            Console.WriteLine(sumElements);
+        }
+    }
+}
