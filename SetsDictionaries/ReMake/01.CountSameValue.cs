e3549dac49a7bc98354b42e41a37a7701dd19b7b
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-04-28 21:35:24 +0300

    100/100

--- SetsDictionaries/ReMake/01.CountSameValue.cs
+++ SetsDictionaries/ReMake/01.CountSameValue.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ConsoleDictionaries
 {
-    class Program
+    class CountSameValue
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double[] inputNumbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(double.Parse)
+                .ToArray();
+            Dictionary<double, int> dictOccurrences = new Dictionary<double, int>();
+
+            foreach (var num in inputNumbers)
+            {
+                if (dictOccurrences.ContainsKey(num))
+                {
+                    dictOccurrences[num]++;
+                }
+                else
+                {
+                    dictOccurrences[num] = 1;
+                }
+            }
+
+            foreach (var num in dictOccurrences)
+            {
+                Console.WriteLine($"{num.Key} - {num.Value} times");
+            }
         }
     }
 }
