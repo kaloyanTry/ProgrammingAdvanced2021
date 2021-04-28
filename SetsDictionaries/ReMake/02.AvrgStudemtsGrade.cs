d10fb25939b86e42a4f2ea735750bed2670092bf
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-04-28 21:49:16 +0300

    100/100

---
+++ SetsDictionaries/ReMake/02.AvrgStudemtsGrade.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.AvrgStudentsGrade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, List<double>> students = new Dictionary<string, List<double>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                string name = input[0];
+                double grade = double.Parse(input[1]);
+
+                if (!students.ContainsKey(name))
+                {
+                    students.Add(name, new List<double>());
+                }
+
+                students[name].Add(grade);
+            }
+
+            foreach (var student in students)
+            {
+                Console.Write($"{student.Key} -> ");
+
+                foreach (var grade in student.Value)
+                {
+                    Console.Write($"{grade:F2} ");
+                }
+
+                Console.WriteLine($"(avg: {student.Value.Average():F2})");
+            }
+        }
+    }
+}
