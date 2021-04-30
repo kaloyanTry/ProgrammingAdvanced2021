759e0721097477b9b94ad386c325d1b2f581c0c1
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-04-30 07:42:53 +0300

    100/100

---
+++ SetsDictionaries/ReMake/Ex03.PeriodicTable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ex03.PeriodicTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            HashSet<string> elementsSet = new HashSet<string>();
+            
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] elements = Console.ReadLine().Split();
+
+                foreach (var element in elements)
+                {
+                    elementsSet.Add(element);
+                }
+            }
+
+            elementsSet = elementsSet.OrderBy(n => n).ToHashSet();
+
+            Console.WriteLine(string.Join(" ", elementsSet));
+
+        }
+    }
+}
