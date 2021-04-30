7cd6479bf731f7aa06237a0c37bb977bdd4d9ce1
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-04-30 08:01:09 +0300

    100/100

---
+++ SetsDictionaries/ReMake/Ex04.DictEvenTimes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04.DictEvenTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<int, int> numsDict = new Dictionary<int, int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (!numsDict.ContainsKey(num))
+                {
+                    numsDict[num] = 0;
+                }
+
+                numsDict[num]++;
+            }
+
+            foreach (var num in numsDict)
+            {
+                if (num.Value % 2 == 0)
+                {
+                    Console.WriteLine(num.Key);
+                }
+            }
+        }
+    }
+}
