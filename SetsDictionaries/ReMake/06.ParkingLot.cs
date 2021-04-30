e32318dfb7ffbaa0a712b7c18a9e4d09f67e77b6
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-04-30 06:31:37 +0300

    100/100

---
+++ SetsDictionaries/ReMake/06.ParkingLot.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06.ParkingLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            HashSet<string> carsSet = new HashSet<string>();
+
+            while (input != "END")
+            {
+                string[] data = input.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+                string cmd = data[0];
+                string carNumber = data[1];
+
+                if (cmd == "IN")
+                {
+                    carsSet.Add(carNumber);
+                }
+
+                if (cmd == "OUT")
+                {
+                    carsSet.Remove(carNumber);
+                }
+
+
+                input = Console.ReadLine();
+            }
+
+            if (carsSet.Any())
+            {
+                foreach (var car in carsSet)
+                {
+                    Console.WriteLine(car);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+        }
+    }
+}
