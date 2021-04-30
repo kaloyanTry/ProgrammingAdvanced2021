4fe9501e1eee38d76ee04878d7d6db4b2c44de69
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-04-30 06:21:44 +0300

    100/100

---
+++ SetsDictionaries/ReMake/05.HashSetUniqueNames.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05.HashSetUniqueNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            HashSet<string> namesSet = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                namesSet.Add(name);
+            }
+
+            foreach (var name in namesSet)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
