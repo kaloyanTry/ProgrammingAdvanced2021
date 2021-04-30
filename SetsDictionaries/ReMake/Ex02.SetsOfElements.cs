079c7f2da4beb11a02125b73b94102cb73ba89f5
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-04-30 07:22:45 +0300

    100/100

---
+++ SetsDictionaries/ReMake/Ex02.SetsOfElements.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ex02.SetsOfElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputs = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            HashSet<int> firstSet = new HashSet<int>();
+            HashSet<int> secondSet = new HashSet<int>();
+
+            for (int i = 0; i < inputs[0]; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                firstSet.Add(num);
+            }
+
+            for (int j = 0; j < inputs[1]; j++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                secondSet.Add(num);
+            }
+
+            firstSet.IntersectWith(secondSet);
+            Console.WriteLine(string.Join(" ", firstSet));
+        }
+    }
+}
