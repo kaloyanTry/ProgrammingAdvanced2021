f642e2e5905c1c1fbd11a5fca25bba4bc4ac9b9a
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-04-30 07:02:26 +0300

    100/100

---
+++ SetsDictionaries/ReMake/07.SoftUniParty.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.SoftUniParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> vipTypes = new HashSet<string>();
+            HashSet<string> regularTypes = new HashSet<string>();
+
+            string input = Console.ReadLine();
+            while (input != "PARTY")
+            {
+                if (input[0] >= '0' && input[0] <= '9')
+                {
+                    vipTypes.Add(input);
+                }
+                else
+                {
+                    regularTypes.Add(input);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            int allTypes = vipTypes.Count + regularTypes.Count;
+
+            input = Console.ReadLine();
+            while (input != "END")
+            {
+                if (input[0] >= '0' && input[0] <= '9')
+                {
+                    vipTypes.Remove(input);
+                    allTypes--;
+                }
+                else
+                {
+                    regularTypes.Remove(input);
+                    allTypes--;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(allTypes);
+
+            if (vipTypes.Any())
+            {
+                foreach (var vip in vipTypes)
+                {
+                    Console.WriteLine(vip);
+                }
+            }
+
+            if (regularTypes.Any())
+            {
+                foreach (var regular in regularTypes)
+                {
+                    Console.WriteLine(regular);
+                }
+            }
+        }
+    }
+}
