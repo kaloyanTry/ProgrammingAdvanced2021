317fe2fc9aef3d30da8563e4f9142a76ff1265a1
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-05-03 07:47:30 +0300

    100/100

---
+++ SetsDictionaries/ReMake/Ex05.CountSymbols.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ex05.CountSymbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Dictionary<char, int> charsDict = new Dictionary<char, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char currentChar = input[i];
+
+                if (!charsDict.ContainsKey(currentChar))
+                {
+                    charsDict[currentChar] = 1;
+                }
+                else
+                {
+                    charsDict[currentChar]++;
+                }
+            }
+
+            foreach (var ch in charsDict.OrderBy(c => c.Key))
+            {
+                Console.WriteLine($"{ch.Key}: {ch.Value} time/s");
+            }
+        }
+    }
+}
