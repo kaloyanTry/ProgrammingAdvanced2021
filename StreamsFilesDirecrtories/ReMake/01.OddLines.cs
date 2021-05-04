c676717a594f91c8da9de8ba46116455c219a289
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-05-04 11:23:46 +0300

    100/100

---
+++ StreamsFilesDirecrtories/ReMake/01.OddLines.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace _01.OddLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("../../../input.txt"))
+            {
+                string readCurrentRow = reader.ReadLine();
+                int currentRow = 0;
+
+                using (StreamWriter writer = new StreamWriter("../../../output.txt"))
+                {
+                    while (readCurrentRow != null)
+                    {
+                        if (currentRow % 2 == 1)
+                        {
+                            writer.WriteLine(readCurrentRow);
+                        }
+
+                        readCurrentRow = reader.ReadLine();
+                        currentRow++;
+                    }
+                }
+            }
+        }
+    }
+}
