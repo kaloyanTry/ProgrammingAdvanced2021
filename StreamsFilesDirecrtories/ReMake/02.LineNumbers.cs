f718c8417fdca4a93b428a4939ea45b88cae6375
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-05-04 11:34:57 +0300

    100/100

---
+++ StreamsFilesDirecrtories/ReMake/02.LineNumbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace _02.LineNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("../../../Input.txt") )
+            {
+                using (StreamWriter writer = new StreamWriter("../../../Output.txt"))
+                {
+                    string readerLine = reader.ReadLine();
+                    int currentRow = 1;
+
+                    while (readerLine != null)
+                    {
+                        writer.WriteLine($"{currentRow}. {readerLine}");
+
+                        currentRow++;
+                        readerLine = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
