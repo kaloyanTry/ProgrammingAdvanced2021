5378dc4bd8cb8a3342dbf3f4ca692345bc1c6a41
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-05-09 11:59:29 +0300

    100/100

---
+++ StreamsFilesDirecrtories/ReMake/03.WordCount.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _03.WordCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> wordsDict = new Dictionary<string, int>();
+ 
+            using (StreamReader reader = new StreamReader("../../../words.txt"))
+            {
+                string inputWords = reader.ReadLine();
+
+                while (inputWords != null)
+                {
+                    string[] wordsLine = inputWords.Split().Select(w => w.ToLower()).ToArray();
+
+                    foreach (var word in wordsLine)
+                    {
+                        if (!wordsDict.ContainsKey(word))
+                        {
+                            wordsDict[word] = 0;
+                        }
+                    }
+
+                    inputWords = reader.ReadLine();
+                }
+            }
+
+            using (StreamReader reader = new StreamReader("../../../text.txt"))
+            {
+                string inputText = reader.ReadLine();
+
+                while (inputText != null)
+                {
+                    string[] textWords = inputText.Split()
+                        .Select(w => w.ToLower())
+                        .Select(w => w.TrimStart(new[] { '-', ',', '.', '!', '?' }))
+                        .Select(w => w.TrimEnd(new[] { '-', ',', '.', '!', '?' }))
+                        .ToArray();
+
+                    foreach (var word in textWords)
+                    {
+                        if (wordsDict.ContainsKey(word))
+                        {
+                            wordsDict[word]++;
+                        }
+                    }
+
+                    inputText = reader.ReadLine();
+                }
+            }
+
+            using (StreamWriter writer = new StreamWriter("../../../output.txt"))
+            {
+                foreach (var pair in wordsDict.OrderByDescending(w => w.Value))
+                {
+                    writer.WriteLine($"{pair.Key} - {pair.Value}");
+                }
+            }
+        }
+
+    }
+}
