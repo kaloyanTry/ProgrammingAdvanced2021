6eba51c34314662f7cbe8c49fdcb25313e2aaed0
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-05-09 16:43:15 +0300

    100/100

---
+++ StreamsFilesDirecrtories/ReMake/06.FolderSize.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace _05.FolderSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] files = Directory.GetFiles("../../../TestFolder");
+
+            double totalSizeMB = 0;
+
+            foreach (var file in files)
+            {
+                FileInfo fileInfo = new FileInfo(file);
+                double fileSizeMB = (double) fileInfo.Length / 1024 / 1024;
+                totalSizeMB += fileSizeMB;
+            }
+
+            File.WriteAllText("../../../Output.txt", totalSizeMB.ToString());
+        }
+    }
+}
