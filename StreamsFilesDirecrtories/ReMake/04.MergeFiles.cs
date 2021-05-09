9eb99cd2fd3d3314229dff2777906524f1f513e9
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-05-09 16:15:46 +0300

    100/100

---
+++ StreamsFilesDirecrtories/ReMake/04.MergeFiles.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _04.MergeFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numsList = new List<int>();
+
+            using (StreamReader reader = new StreamReader("../../../FileOne.txt"))
+            {
+                string inputLine = reader.ReadLine();
+                while (inputLine != null)
+                {
+                    int num = int.Parse(inputLine);
+                    numsList.Add(num);
+
+                    inputLine = reader.ReadLine();
+                }
+            }
+
+            using (StreamReader reader = new StreamReader("../../../FileTwo.txt"))
+            {
+                string inputLine = reader.ReadLine();
+                while (inputLine != null)
+                {
+                    int num = int.Parse(inputLine);
+                    numsList.Add(num);
+
+                    inputLine = reader.ReadLine();
+                }
+            }
+
+            using (StreamWriter writer = new StreamWriter("../../../Output.txt"))
+            {
+                foreach (var num in numsList)
+                {
+                    writer.WriteLine(num);
+                }
+            }
+        }
+    }
+}
