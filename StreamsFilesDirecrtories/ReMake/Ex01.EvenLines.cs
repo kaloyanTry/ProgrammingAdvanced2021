10c393c903c8a9e407f5077dd3c365a88d1f471f
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-05-10 18:38:44 +0300

    100/100

---
+++ StreamsFilesDirecrtories/ReMake/Ex01.EvenLines.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Ex01.EvenLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathInput = "../../../text.txt";
+            string pathOutput = "../../../output.txt";
+
+            using (StreamReader reader = new StreamReader(pathInput))
+            {
+                using (StreamWriter writer = new StreamWriter(pathOutput))
+                {
+                    string readerLine = reader.ReadLine();
+                    int readerRow = 0;
+
+                    while (readerLine != null)
+                    {
+                        if (readerRow++ % 2 == 0)
+                        {
+                            string[] words = readerLine.Split().Reverse()
+                                .Select(w => w.Replace('-', '@'))
+                                .Select(w => w.Replace(',', '@'))
+                                .Select(w => w.Replace('.', '@'))
+                                .Select(w => w.Replace('!', '@'))
+                                .Select(w => w.Replace('?', '@')).ToArray();
+
+                            writer.WriteLine(string.Join(" ", words));
+                        }
+
+                        readerLine = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
