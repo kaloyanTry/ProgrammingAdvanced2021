a6823bace1812e8a2ccb0324b61a971974e8dfcc
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-05-10 19:37:46 +0300

    100/100

---
+++ StreamsFilesDirecrtories/ReMake/Ex02.LineNumbers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Ex02.LineNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathText = "../../../text.txt";
+            string pathResult = "../../../actualResult.txt";
+
+            string[] readerAllLinesText = File.ReadAllLines(pathText);
+            //List<char> marksList = new List<char>() {',', '-', '.', '!', '?', '\''};
+
+            for (int i = 0; i < readerAllLinesText.Length; i++)
+            {
+                string readerLine = readerAllLinesText[i];
+
+                int lettersCount = 0;
+                int marksCount = 0;
+
+                foreach (var ch in readerLine)
+                {
+                    if (char.IsLetter(ch))
+                    {
+                        lettersCount++;
+                    }
+                    else if (char.IsPunctuation(ch))
+                    {
+                        marksCount++;
+                    }
+                }
+
+                string writerLine = $"Line {i + 1}: {readerLine} ({lettersCount})({marksCount})";
+
+                File.AppendAllText((pathResult), writerLine + Environment.NewLine);
+            }
+        }
+    }
+}
