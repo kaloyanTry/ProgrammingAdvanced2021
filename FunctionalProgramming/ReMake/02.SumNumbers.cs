5eac39cd233ee143ce210c275978fefef50e5b3b
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-05-11 19:30:55 +0300

    100/100

---
+++ FunctionalProgramming/ReMake/02.SumNumbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputNumbers = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            Console.WriteLine(inputNumbers.Length);
+            Console.WriteLine(inputNumbers.Sum());
+        }
+    }
+}
