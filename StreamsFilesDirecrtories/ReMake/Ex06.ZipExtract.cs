bf369327d0f03237e0093e0195a4d1c8197c9fbe
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-05-11 16:49:21 +0300

    100/100

---
+++ StreamsFilesDirecrtories/ReMake/Ex06.ZipExtract.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.IO.Compression;
+
+namespace Ex06.ZipExtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var zipFile = "../../../myzip.zip";
+            var file = "../../../copyMe.png";
+
+            using (var archive = ZipFile.Open(zipFile, ZipArchiveMode.Create))
+            {
+                archive.CreateEntryFromFile(file, Path.GetFileName(file));
+            }
+        }
+    }
+}
