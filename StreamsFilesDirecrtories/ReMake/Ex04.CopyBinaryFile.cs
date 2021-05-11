b8f2e9c93ff5bd4b533bcfff84d403dc4ef1cade
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-05-11 16:35:28 +0300

    100/100

---
+++ StreamsFilesDirecrtories/ReMake/Ex04.CopyBinaryFile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Ex04.CopyBinaryFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream readStream = new FileStream("../../../copyMe.png", FileMode.Open))
+            {
+                using (FileStream writeStream = new FileStream("../../../copiedImage.png", FileMode.Create))
+                {
+                    while (true)
+                    {
+                        byte[] bytesBuffer = new byte[4096];
+                        int readBuffer = readStream.Read(bytesBuffer, 0, bytesBuffer.Length);
+
+                        if (readBuffer == 0)
+                        {
+                            break;
+                        }
+
+                        writeStream.Write(bytesBuffer, 0, readBuffer);
+                    }
+                }
+            }
+        }
+    }
+}
