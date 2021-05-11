30f34deb37fb9aceb9510d665aee5a10369df78e
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-05-11 21:15:08 +0300

    100/100

---
+++ FunctionalProgramming/ReMake/03.CountUppercaseWords.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.CountUppercaseWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputText = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            List<string> upperWordsList = new List<string>();
+
+            foreach (var word in inputText)
+            {
+                char firstLetter = word[0];
+                if (char.IsUpper(firstLetter))
+                {
+                    upperWordsList.Add(word);
+                }
+            }
+
+            foreach (var word in upperWordsList)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
