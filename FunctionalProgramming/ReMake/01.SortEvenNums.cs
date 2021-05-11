4db852b57f04bad4db311f0ef6433d540116753f
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-05-11 19:26:00 +0300

    100/100

---
+++ FunctionalProgramming/ReMake/01.SortEvenNums.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleFunctional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArray = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            int[] orderedArray = inputArray.Where(n => n % 2 == 0).OrderBy(n => n).ToArray();
+
+            Console.WriteLine(string.Join(", ", orderedArray));
+        }
+    }
+}
