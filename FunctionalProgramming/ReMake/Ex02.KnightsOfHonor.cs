beb06a3e66dba8b272188c7c8260808dda171d5a
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-05-12 21:24:10 +0300

    100/100

---
+++ FunctionalProgramming/ReMake/Ex02.KnightsOfHonor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ex02.KnightsOfHonor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> inputNames = Console.ReadLine().Split().ToList();
+
+            Action<List<string>> printKnights = x => Console.WriteLine(string.Join(Environment.NewLine, x.Select(y => $"Sir {y}")));
+
+            printKnights(inputNames);
+        }
+    }
+}
