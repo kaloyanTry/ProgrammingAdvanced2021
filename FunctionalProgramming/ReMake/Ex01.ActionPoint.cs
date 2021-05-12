d31fa31e505e80f91bf40d2b8523c53c46ec3312
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-05-12 21:23:07 +0300

    100/100

---
+++ FunctionalProgramming/ReMake/Ex01.ActionPoint.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ex01.ActionPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> inputNames = Console.ReadLine().Split().ToList();
+
+            Action<string> printNames = x => Console.WriteLine(x);
+
+            inputNames.ForEach(printNames);
+        }
+    }
+}
