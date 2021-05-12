7e4d94d678c5f257d3905df59775573378f1cb64
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-05-12 15:30:13 +0300

    100/100

---
+++ FunctionalProgramming/ReMake/04.AddVat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.AddVat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal[] inputPrices = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(decimal.Parse).ToArray();
+
+            decimal[] vatPrices = inputPrices.Select(p => p * 1.2m).ToArray();
+
+            foreach (var price in vatPrices)
+            {
+                Console.WriteLine($"{price:F2}");
+            }
+        }
+    }
+}
