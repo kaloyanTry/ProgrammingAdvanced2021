fa5206ed4a1c3df7be8ed74cdb9af15239b284f2
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-05-21 09:26:00 +0300

    100/100

--- Matrix/ReMake/5.SquareMaxSum.cs
+++ Matrix/ReMake/5.SquareMaxSum.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Linq;
 
 namespace _5.SquareMaxSum
@@ -7,30 +7,32 @@ namespace _5.SquareMaxSum
     {
         static void Main(string[] args)
         {
-            int[] size = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
-            int mRow = size[0];
-            int mCol = size[1];
+            int[] sizes = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int sizeRow = sizes[0];
+            int sizeCol = sizes[1];
+            int[,] matrix = new int[sizeRow, sizeCol];
 
-            int[,] matrix = new int[mRow, mCol];
-            int maxRow = -1;
-            int maxCol = -1;
-            int maxSum = int.MinValue;
 
-            for (int row = 0; row < mRow; row++)
+            for (int row = 0; row < matrix.GetLength(0); row++)
             {
                 int[] data = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
 
-                for (int col = 0; col < mCol; col++)
+                for (int col = 0; col < matrix.GetLength(1); col++)
                 {
-                    matrix[row, col] = data[col];
+                    matrix[row, col] += data[col];
                 }
             }
+
+            int maxSum = int.MinValue;
+            int maxRow = -1;
+            int maxCol = -1;
 
             for (int row = 0; row < matrix.GetLength(0) - 1; row++)
             {
                 for (int col = 0; col < matrix.GetLength(1) - 1; col++)
                 {
                     int currentSum = matrix[row, col] + matrix[row, col + 1] + matrix[row + 1, col] + matrix[row + 1, col + 1];
+                    
                     if (currentSum > maxSum)
                     {
                         maxSum = currentSum;
