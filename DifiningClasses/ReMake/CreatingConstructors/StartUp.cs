7d863ef2abffd146ae2f86f82eb2af067dd04a04
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-06-03 21:26:29 +0300

    100/100

---
+++ DifiningClasses/ReMake/CreatingConstructors/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            Person person = new Person(name, age);
+        }
+    }
+}
