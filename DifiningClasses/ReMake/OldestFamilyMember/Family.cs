bbe6d63f17dbbb06dc5bac85ce380d3fa695eb83
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-06-03 21:27:54 +0300

    100/100

---
+++ DifiningClasses/ReMake/OldestFamilyMember/Family.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        private List<Person> members;
+
+        public Family()
+        {
+            Members = new List<Person>();
+        }
+
+        public List<Person> Members { get; set; }
+
+        public void AddMember(Person member)
+        {
+            Members.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            return Members.OrderByDescending(m => m.Age).FirstOrDefault();
+        }
+    }
+}
---
+++ DifiningClasses/ReMake/OldestFamilyMember/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Family family = new Family();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] inputData = Console.ReadLine().Split();
+                string name = inputData[0];
+                int age = int.Parse(inputData[1]);
+
+                Person person = new Person(name, age);
+                family.AddMember(person);
+            }
+
+            Console.WriteLine(family.GetOldestMember().ToString());
+        }
+    }
+}
