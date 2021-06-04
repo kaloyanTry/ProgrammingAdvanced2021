84ca5791aa51f9d9562ce7970ac63e71adf464ce
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-06-04 18:38:55 +0300

    100/100

---
+++ DifiningClasses/ReMake/OpinionPoll/Person.cs
@@ -0,0 +1,33 @@
+﻿namespace DefiningClasses
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person()
+        {
+            Name = "No name";
+            Age = 1;
+        }
+
+        public Person(int age) : this()
+        {
+            Age = age;
+        }
+
+        public Person(string name, int age) : this(age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Age}";
+        }
+    }
+}
---
+++ DifiningClasses/ReMake/OpinionPoll/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Person> persons = new List<Person>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] inputData = Console.ReadLine().Split();
+                string name = inputData[0];
+                int age = int.Parse(inputData[1]);
+
+                Person person = new Person(name, age);
+                persons.Add(person);
+
+            }
+
+            foreach (var person in persons.Where(p => p.Age > 30).OrderBy(p => p.Name))
+            {
+                Console.WriteLine(person);
+            }
+        }
+    }
+}
