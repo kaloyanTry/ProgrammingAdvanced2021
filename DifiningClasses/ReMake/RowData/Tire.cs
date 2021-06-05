53cfcd428e08dccffebd3f9a84f7e77a6fd5c032
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-06-05 07:51:31 +0300

    100/100

---
+++ DifiningClasses/ReMake/RowData/Tire.cs
@@ -0,0 +1,15 @@
+﻿namespace RawData
+{
+    public class Tire
+    {
+        public Tire(double tirePressure, int tireAge)
+        {
+            TirePressure = tirePressure;
+            TireAge = tireAge;
+        }
+
+        public double TirePressure { get; set; }
+        public int TireAge { get; set; }
+        public int Pressure { get; internal set; }
+    }
+}
