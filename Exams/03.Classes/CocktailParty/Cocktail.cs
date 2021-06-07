ca5edbed624718593c994cc3662df975b3e702ff
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-06-07 22:11:15 +0300

    100/100

---
+++ Exams/03.Classes/CocktailParty/Cocktail.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CocktailParty
+{
+    public class Cocktail
+    {
+        private List<Ingredient> Ingredients;
+
+        public Cocktail(string name, int capacity, int maxAlcoholLevel)
+        {
+            Name = name;
+            Capacity = capacity;
+            MaxAlcoholLevel = maxAlcoholLevel;
+            Ingredients = new List<Ingredient>();
+        }
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int MaxAlcoholLevel { get; set; }
+
+        public void Add(Ingredient ingredient)
+        {
+            Ingredient currIngredient = Ingredients.FirstOrDefault(i => i.Name == ingredient.Name);
+
+            if (currIngredient == null && ingredient.Quantity < Capacity && ingredient.Alcohol < MaxAlcoholLevel)
+            {
+                Ingredients.Add(ingredient);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            Ingredient ingredient = Ingredients.FirstOrDefault(i => i.Name == name);
+            if (ingredient == null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public Ingredient FindIngredient(string name)
+        {
+            Ingredient ingredient = Ingredients.FirstOrDefault(i => i.Name == name);
+            if (ingredient == null)
+            {
+                return null;
+            }
+
+            return ingredient;
+        }
+
+        public Ingredient GetMostAlcoholicIngredient()
+        {
+            Ingredient mostAlcoholicIngredient = Ingredients.OrderByDescending(i => i.Alcohol).FirstOrDefault();
+
+            return mostAlcoholicIngredient;
+        }
+
+        //public int CurrentAlcoholLevel()
+        //{
+        //    int amountAlcohol = 0;
+
+        //    foreach (var coctail in Ingredients)
+        //    {
+        //        amountAlcohol += coctail.Alcohol;
+        //    }
+
+        //    return amountAlcohol;
+        //}
+
+        public int CurrentAlcoholLevel => Ingredients.Capacity;
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Cocktail: {Name} - Current Alcohol Level: {CurrentAlcoholLevel}");
+
+            foreach (var ingredient in Ingredients)
+            {
+                Console.WriteLine(ingredient);
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ Exams/03.Classes/CocktailParty/Ingredient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CocktailParty
+{
+    public class Ingredient
+    {
+        public Ingredient(string name, int alcohol, int quantity)
+        {
+            Name = name;
+            Alcohol = alcohol;
+            Quantity = quantity;
+        }
+
+        public string Name { get; set; }
+        public int Alcohol { get; set; }
+        public int Quantity { get; set; }
+
+        public override string ToString()
+        {
+            string result = $"Ingredient: {Name}" + Environment.NewLine +
+                            $"Quantity: {Quantity}" + Environment.NewLine +
+                            $"Alcohol: {Alcohol}";
+
+            return result.Trim();
+        }
+    }
+}
