1259dd6e0034bad66c4ec5f952f1df285b449c56
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-07-24 10:00:35 +0300

    Another smart solution 100/100

---
+++ Exams/01.StacksQueues/01.Scheduling2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Problem
+{
+    class Cooking
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> tasks = new Stack<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            Queue<int> threads = new Queue<int>(Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+            int taskToKill = int.Parse(Console.ReadLine());
+            int lastThread = 0;
+
+            while (true)
+            {
+                int task = tasks.Peek();
+                int thread = threads.Peek();
+
+                if (thread >= task)
+                {
+                    tasks.Pop();
+                    threads.Dequeue();
+                }
+                else
+                {
+                    threads.Dequeue();
+                }
+
+                if (task == taskToKill)
+                {
+                    lastThread = thread;
+                    Console.WriteLine($"Thread with value {thread} killed task {taskToKill}");
+                    break;
+                }
+            }
+
+            Console.WriteLine($"{lastThread} " + string.Join(' ', threads));
+        }
+    }
+}
