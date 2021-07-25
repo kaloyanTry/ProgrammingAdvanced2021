a16084e7a814a512fbc8e45cfc95e1dedacc0f6f
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-07-25 10:59:55 +0300

    Another solution 100/100

---
+++ Matrix/ReMake/SymbolInMatrix2.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleMatrix2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[n, n];
+            bool isChar = false;
+            int chRow = -1;
+            int chCol = - 1;
+
+            for (int row = 0; row < n; row++)
+            {
+                 string rowData = Console.ReadLine();
+
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = rowData[col];
+                }
+            }
+            char ch = char.Parse(Console.ReadLine());
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    if (matrix[row, col] == ch)
+                    {
+                        isChar = true;
+                        chRow = row;
+                        chCol = col;
+                        break;
+                    }
+                }
+
+                if (isChar)
+                {
+                    break;
+                }
+            }
+
+            if (isChar)
+            {
+                Console.WriteLine($"({chRow}, {chCol})");
+            }
+            else
+            {
+                Console.WriteLine($"{ch} does not occur in the matrix");
+            }
+
+            //Print the matrix:
+            //for (int row = 0; row < matrix.GetLength(0); row++)
+            //{
+            //    for (int col = 0; col < matrix.GetLength(1); col++)
+            //    {
+            //        Console.Write(matrix[row, col] + " ");
+            //    }
+            //    Console.WriteLine();
+            //}
+        }
+    }
+}
