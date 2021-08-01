f96a0018993eff4d448441f093a3624430e449bc
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-08-01 17:33:46 +0300

    100/100

---
+++ Exams/03.Classes/Guild/Guild.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Guild
+{
+    public class Guild
+    {
+        private List<Player> roster;
+
+        public Guild(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            roster = new List<Player>();
+        }
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+
+        public int Count => roster.Count;
+
+        public void AddPlayer(Player player)
+        {
+            if (roster.Count < Capacity)
+            {
+                roster.Add(player);
+            }
+        }
+
+        public bool RemovePlayer(string name)
+        {
+            Player player = roster.FirstOrDefault(p => p.Name == name);
+
+            return roster.Remove(player);
+        }
+
+        public void PromotePlayer(string name)
+        {
+            Player player = roster.FirstOrDefault(p => p.Name == name);
+
+            if (player.Rank != "Member")
+            {
+                player.Rank = "Member";
+            }
+        }
+
+        public void DemotePlayer(string name)
+        {
+            Player player = roster.FirstOrDefault(p => p.Name == name);
+
+            if (player.Rank != "Trial")
+            {
+                player.Rank = "Trial";
+            }
+        }
+
+        public Player[] KickPlayersByClass(string classc)
+        {
+            List<Player> kickPlayers = roster.FindAll(p => p.Class == classc);
+            Player[] arrPlayers = kickPlayers.ToArray();
+
+            roster.RemoveAll(p => p.Class == classc);
+
+            return arrPlayers;
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Players in the guild: {Name}");
+
+            foreach (var player in roster)
+            {
+                sb.AppendLine(player.ToString());
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
