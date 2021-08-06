a11d0b536d29e591d693a9ff39609eb6a030b185
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-08-06 16:19:17 +0300

    Fibonacci Recursion

---
+++ DataStructures/Recursion/RacursionFibonacci.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace RacursionFIbonacci
+{
+    class RacursionFibonacci
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Fibonacci(n));
+        }
+
+        private static int Fibonacci(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+
+            if (n == 1 || n == 2)
+            {
+                return 1;
+            }
+
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+}
