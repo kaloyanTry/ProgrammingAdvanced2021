0f9c5c32d66b45257bff5fb7e54f0eb056937ab1
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-08-06 16:22:27 +0300

    Sum array elements
    Sum array elements, index

---
+++ DataStructures/Recursion/RecursionArraySum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RecursionArryaSum
+{
+    class ArraySum
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            Console.WriteLine(Sum(array, 0));
+        }
+
+        static int Sum(int[] array, int index)
+        {
+            if (index == array.Length)
+            {
+                return 0;
+            }
+
+            return array[index] + Sum(array, index + 1);
+        }
+    }
+}
