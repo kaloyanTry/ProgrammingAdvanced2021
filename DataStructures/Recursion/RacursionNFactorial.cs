6d015c5f558235ad82af9d5260dd24cf819b877f
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-08-06 16:20:32 +0300

    RecursionFactorialN

---
+++ DataStructures/Recursion/RacursionNFactorial.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FactoriealN
+{
+    class RacursionNFactorial
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            //NFactorial(n);
+
+            Console.WriteLine(NFactorial(n));
+        }
+
+        private static int NFactorial(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+
+            return n * NFactorial(n - 1);
+        }
+    }
+}
