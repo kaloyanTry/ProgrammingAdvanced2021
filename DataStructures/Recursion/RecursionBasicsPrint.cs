51d95d1fee013946493cd1a87d14fedf3d3de514
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-08-06 16:17:04 +0300

    Basic Print Recursion
    Print(n-1) mehtod

---
+++ DataStructures/Recursion/RecursionBasicsPrint.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class RecursionBasicsPrint
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Print(n);
+        }
+
+        private static void Print(int n)
+        {
+            //Bottom: when the recursion stops:
+            if (n == 0)
+            {
+                return;
+            }
+
+            Console.WriteLine($"{n}: You are simple the best!");
+
+            //The method itself:
+            Print(n - 1);
+        }
+    }
+}
