2b3d0db1693df64c8c7706e3148027798bc2fda5
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-08-07 22:25:00 +0300

    Add files via upload

---
+++ DataStructures/LinearDS/LinkedList/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImplementedLinkedList
+{
+    public class Node<T>
+    {
+        public Node(T value)
+        {
+            Value = value;
+        }
+
+        public T Value { get; set; }
+
+        public Node<T> Next { get; set; }
+    }
+}
