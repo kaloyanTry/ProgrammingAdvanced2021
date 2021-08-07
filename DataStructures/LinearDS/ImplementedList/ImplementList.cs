745629cc2970038b76b1a16cdf43f665a1545197
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-08-07 22:23:35 +0300

    Add files via upload

---
+++ DataStructures/LinearDS/ImplementedList/ImplementList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _02.LinearDS
+{
+    class ImplementList
+    {
+        static void Main(string[] args)
+        {
+            TooCoolList<int> list = new TooCoolList<int>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                list.Add(i);
+
+                Console.WriteLine($"Internal array count: {list.InternalTooCount}");
+                Console.WriteLine($"List count: {list.Count}");
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ DataStructures/LinearDS/ImplementedList/TooCoolList.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02.LinearDS
+{
+    public class TooCoolList<T>
+    {
+        private T[] tooArray;
+        private int tooIndex;
+
+        public TooCoolList(int arrayLenght = 4)
+        {
+            tooArray = new T[arrayLenght];
+        }
+
+        public T this[int i]
+        {
+            get 
+            { 
+                return tooArray[i]; 
+            }
+            set 
+            { 
+                tooArray[i] = value; 
+            }
+        }
+
+
+        public int Count => tooIndex;
+
+        public int InternalTooCount => tooArray.Length;
+
+        public void Add(T element)
+        {
+            if (tooIndex == tooArray.Length)
+            {
+                tooArray = DoubledTooArraySize(tooArray);
+            }
+
+            tooArray[tooIndex++] = element;
+        }
+
+        private T[] DoubledTooArraySize(T[] tooArray)
+        {
+            T[] doubledTooArray = new T[tooArray.Length * 2];
+
+            for (int i = 0; i < tooArray.Length; i++)
+            {
+                doubledTooArray[i] = tooArray[i];
+            }
+
+            return doubledTooArray;
+        }
+    }
+}
