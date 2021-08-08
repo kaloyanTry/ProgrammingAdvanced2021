a3a0d83584757fa96c8ebed9d1b3539e1c8d59b0
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-08-08 08:36:47 +0300

    100/100

---
+++ Exams/01.StacksQueues/01.Scheduling3.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Problem
+{
+    class Brackets2
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> tasks = new Stack<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            Queue<int> threads = new Queue<int>(Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            int taskToKill = int.Parse(Console.ReadLine());
+
+            while (tasks.Any() && threads.Any())
+            {
+                int task = tasks.Peek();
+                int thread = threads.Peek();
+
+                if (task == taskToKill)
+                {
+                    Console.WriteLine($"Thread with value {thread} killed task {taskToKill}");
+                    break;
+                }
+
+                if (thread >= task)
+                {
+                    tasks.Pop();
+                    threads.Dequeue();
+                }
+                else if (thread < task)
+                {
+                    threads.Dequeue();
+                }
+            }
+
+            if (threads.Any())
+            {
+                Console.WriteLine(string.Join(' ', threads));
+            }
+        }
+    }
+}
