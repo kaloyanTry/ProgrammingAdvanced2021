06aed6255e1feaf4cf6f0516cd7ebed47a3ab84a
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-08-11 10:04:08 +0300

    100/100

---
+++ Exams/02.Matrix/02.SuperMario3.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace _02.SuperMario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int livesMario = int.Parse(Console.ReadLine());
+            int nRows = int.Parse(Console.ReadLine());
+
+            char[][] matrix = new char[nRows][];
+            int rowMario = -1;
+            int colMario = -1;
+
+            for (int row = 0; row < nRows; row++)
+            {
+                string inputRow = Console.ReadLine();
+
+                matrix[row] = new char[inputRow.Length];
+
+                for (int col = 0; col < matrix[row].Length; col++)
+                {
+                    matrix[row][col] = inputRow[col];
+
+                    if (matrix[row][col] == 'M')
+                    {
+                        rowMario = row;
+                        colMario = col;
+                    }
+                }
+            }
+
+            while (true)
+            {
+                string[] inputData = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                matrix[rowMario][colMario] = '-';
+
+                char moveCommand = char.Parse(inputData[0]);
+                int rowSpawn = int.Parse(inputData[1]);
+                int colSpawn = int.Parse(inputData[2]);
+
+                matrix[rowSpawn][colSpawn] = 'B';
+
+                if (moveCommand == 'W')
+                {
+                    rowMario--;
+                }
+                else if (moveCommand == 'S')
+                {
+                    rowMario++;
+                }
+                else if (moveCommand == 'A')
+                {
+                    colMario--;
+                }
+                else if (moveCommand == 'D')
+                {
+                    colMario++;
+                }
+
+                if (rowMario == -1)
+                {
+                    rowMario = 0;
+                }
+                else if (rowMario == nRows)
+                {
+                    rowMario = nRows - 1;
+                }
+                else if (colMario == -1)
+                {
+                    colMario = 0;
+                }
+                else if (colMario == matrix.Length)
+                {
+                    colMario = matrix.Length - 1;
+                }
+
+                livesMario--;
+
+                if (matrix[rowMario][colMario] == matrix[rowSpawn][colSpawn])
+                {
+                    livesMario -= 2;
+                    
+                }
+
+                if (matrix[rowMario][colMario] == 'P')
+                {
+                    matrix[rowMario][colMario] = '-';
+                    Console.WriteLine($"Mario has successfully saved the princess! Lives left: {livesMario}");
+                    break;
+                }
+
+                if (livesMario <= 0)
+                {
+                    matrix[rowMario][colMario] = 'X';
+                    Console.WriteLine($"Mario died at {rowMario};{colMario}.");
+                    break;
+                }
+
+                matrix[rowMario][colMario] = 'M';
+            }
+
+
+            foreach (var row in matrix)
+            {
+                Console.WriteLine(row);
+            }
+
+        }
+    }
+}
