4206cceb514261f8a87b7dd3017b182b83b55607
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-08-12 08:02:13 +0300

    Update Person .cs

--- DifiningClasses/OldestFamilyMemeber/Person .cs
+++ DifiningClasses/OldestFamilyMemeber/Person .cs
@@ -11,10 +11,12 @@ namespace DefiningClasses
             Name = "No name";
             Age = 1;
         }
+        
         public Person(int age) : this()
         {
             Age = age;
         }
+        
         public Person(string name, int age) : this(age)
         {
             Name = name;
