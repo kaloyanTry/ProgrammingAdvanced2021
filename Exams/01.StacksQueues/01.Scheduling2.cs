06d458a36ca808c2fe516509ab46f12e55339994
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-08-15 14:35:15 +0300

    100/100
    Simple solution

--- Exams/01.StacksQueues/01.Scheduling2.cs
+++ Exams/01.StacksQueues/01.Scheduling2.cs
@@ -1,43 +1,41 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace _01.Problem
+namespace _01.Scheduling
 {
-    class Cooking
+    class Program
     {
         static void Main(string[] args)
         {
             Stack<int> tasks = new Stack<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
-            Queue<int> threads = new Queue<int>(Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
-
+            Queue<int> threads = new Queue<int>(Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse));
             int taskToKill = int.Parse(Console.ReadLine());
-            int lastThread = 0;
 
-            while (true)
+            while (threads.Any() && tasks.Any())
             {
-                int task = tasks.Peek();
                 int thread = threads.Peek();
+                int task = tasks.Peek();
+
+                if (task == taskToKill)
+                {
+                    Console.WriteLine($"Thread with value {thread} killed task {taskToKill}");
+                    break;
+                }
 
                 if (thread >= task)
                 {
-                    tasks.Pop();
                     threads.Dequeue();
+                    tasks.Pop();
                 }
                 else
                 {
                     threads.Dequeue();
                 }
-
-                if (task == taskToKill)
-                {
-                    lastThread = thread;
-                    Console.WriteLine($"Thread with value {thread} killed task {taskToKill}");
-                    break;
-                }
             }
 
-            Console.WriteLine($"{lastThread} " + string.Join(' ', threads));
+            Console.WriteLine(string.Join(' ', threads));
         }
     }
 }
