802afee82498e969d9d3061c93d2a38eeee81fe4
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-08-15 08:25:24 +0300

    Update Car.cs

--- Exams/03.Classes/Parking/Car.cs
+++ Exams/03.Classes/Parking/Car.cs
@@ -14,7 +14,9 @@ namespace Parking
         }
 
         public string Manufacturer { get; set; }
+        
         public string Model { get; set; }
+        
         public int Year { get; set; }
 
         public override string ToString()
