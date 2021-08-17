1854954a962117a65a69d2a5baaa39ec86c94083
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-08-17 09:08:21 +0300

    Basic operations with directories and files

---
+++ StreamsFilesDirecrtories/DirectoryFile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleStreams
+{
+    class DirectoryFile
+    {
+        static void Main(string[] args)
+        {
+            Directory.CreateDirectory("../../../DirNew");
+
+            File.Create("../../../DirNew/textNew.txt");
+            File.Create("../../../DirNew/presiNew.ppt");
+
+            string[] filesDir = Directory.GetFiles("../../../DirNew");
+
+            foreach (var file in filesDir)
+            {
+                Console.WriteLine(file);
+
+                //Do the same thing:
+                //FileInfo fileInfo = new FileInfo(file);
+                //Console.WriteLine(fileInfo);
+            }
+
+            //Delete directory:
+            //Directory.Delete("../../../DirNew/Test2Dir", true);
+        }
+    }
+}
