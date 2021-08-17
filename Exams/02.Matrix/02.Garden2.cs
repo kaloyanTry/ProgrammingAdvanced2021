9c1ec2a73f6920d6986693884473e99baaddc788
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-08-17 18:43:49 +0300

    100/100

--- Exams/02.Matrix/02.Garden2.cs
+++ Exams/02.Matrix/02.Garden2.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Linq;
 
 namespace _02.ProblemMatrix2
@@ -7,51 +7,51 @@ namespace _02.ProblemMatrix2
     {
         static void Main(string[] args)
         {
-           int[] inputDimensions = Console.ReadLine().Split().Select(int.Parse).ToArray();
-            int n1 = inputDimensions[0];
-            int n2 = inputDimensions[1];
-            int[,] matrix = new int[n1, n2];
-            int emptyFlower = 0;
+            int[] dimensions = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int dimensionRow = dimensions[0];
+            int dimensionCol = dimensions[1];
 
-            for (int row = 0; row < matrix.GetLength(0); row++)
+            int[,] garden = new int[dimensionRow, dimensionCol];
+
+            for (int row = 0; row < garden.GetLength(0); row++)
             {
-                for (int col = 0; col < matrix.GetLength(1); col++)
+                for (int col = 0; col < garden.GetLength(1); col++)
                 {
-                    matrix[row, col] = emptyFlower;
+                    garden[row, col] = 0;
                 }
             }
 
-            string command = Console.ReadLine();
-            while (command != "Bloom Bloom Plow")
+            string input = Console.ReadLine();
+            while (input != "Bloom Bloom Plow")
             {
-                int[] inputCoordinates = command.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
-                int inputRow = inputCoordinates[0];
-                int inputCol = inputCoordinates[1];
+                int[] dimensionsInput = input.Split().Select(int.Parse).ToArray();
+                int inputRow = dimensionsInput[0];
+                int inputCol = dimensionsInput[1];
 
-                if (inputRow < 0 || inputRow >= matrix.GetLength(0) || inputCol < 0 || inputCol >= matrix.GetLength(1))
+                if (inputRow < 0 || inputRow >= garden.GetLength(0) || inputCol < 0 || inputCol >= garden.GetLength(1))
                 {
                     Console.WriteLine("Invalid coordinates.");
                 }
 
-                for (int row = 0; row < matrix.GetLength(0); row++)
+                for (int row = 0; row < garden.GetLength(0); row++)
                 {
-                    for (int col = 0; col < matrix.GetLength(1); col++)
+                    for (int col = 0; col < garden.GetLength(1); col++)
                     {
-                        if (row == inputRow || col == inputCol)
+                        if (inputRow == row || inputCol == col)
                         {
-                            matrix[row, col]++;
+                            garden[row, col]++;
                         }
                     }
                 }
 
-                command = Console.ReadLine();
+                input = Console.ReadLine();
             }
 
-            for (int row = 0; row < matrix.GetLength(0); row++)
+            for (int row = 0; row < garden.GetLength(0); row++)
             {
-                for (int col = 0; col < matrix.GetLength(1); col++)
+                for (int col = 0; col < garden.GetLength(1); col++)
                 {
-                    Console.Write(matrix[row, col] + " ");
+                    Console.Write(garden[row, col] + " ");
                 }
                 Console.WriteLine();
             }
